4d24efd6c0d30b8cc0b13e73edbc972314396273
SangYupKim <kylekim95@gmail.com>
2025-03-31 15:14:00 +0900

    feat: initial setup

---
+++ src/types/communityPosts.ts
@@ -0,0 +1,12 @@
+type communityPost = {
+  userName: string;
+  userAvatar: string;
+  title: string;
+  description: string;
+  tags: string[];
+  numLikes: number;
+  numComments: number;
+  imageUrl: string;
+};
+
+export default communityPost;
