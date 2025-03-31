64c7eb210223e248428d08c2864c3c676d15b428
jihye choi <jihyec22@gmail.com>
2025-03-31 18:52:11 +0900

    Feat: 픽셀 캐릭터 모션 컴포넌트

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,15 @@
+import PixelCharacter from '@/components/PixelCharacter/PixelCharacter';
+
 export default function Home() {
   return (
     <div>
       <p className=" text-center text-5xl p-5">Play ON!</p>
+      <div className="flex">
+        <PixelCharacter char="mage" motion="attack" />
+        <PixelCharacter char="mage" motion="run" />
+      </div>
+      <PixelCharacter char="archer" motion="run" />
+      <PixelCharacter char="warrior" motion="ulti" />
     </div>
   );
 }
