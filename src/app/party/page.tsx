7781102af80920addcbf3e3701b7a71e3da954c1
marie4053 <marie4053@naver.com>
2025-03-31 20:11:51 +0900

    chore: 불필요한 코드 제거

--- src/app/party/page.tsx
+++ src/app/party/page.tsx
@@ -2,11 +2,8 @@ import PartyCard from '@/components/party/PartyCard';
 
 export default function Party() {
   return (
-    <div className="px-8">
+    <div>
       <p className="text-center text-5xl p-5">Party</p>
-      <div>
-        <PartyCard />
-      </div>
     </div>
   );
 }
