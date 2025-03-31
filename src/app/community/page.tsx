b15f4c4a42f4a95605c018c716dbbf977d11246c
SangYupKim <kylekim95@gmail.com>
2025-03-31 15:14:00 +0900

    feat: initial setup

--- src/app/community/page.tsx
+++ src/app/community/page.tsx
@@ -1,7 +1,22 @@
+import CommunityPostImageLong from '@/components/community/post-image-long';
+import communityPost from '@/types/communityPosts';
+
 export default function Community() {
+  const dummy: communityPost = {
+    userName: '',
+    description: '',
+    imageUrl: '',
+    numComments: 10,
+    numLikes: 50,
+    tags: [],
+    title: '',
+    userAvatar: '',
+  };
+
   return (
     <div>
       <p className="text-center text-5xl p-5">Community</p>
+      <CommunityPostImageLong data={dummy} className="" />
     </div>
   );
 }
