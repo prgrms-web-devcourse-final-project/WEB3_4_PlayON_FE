d69b9c4c8804bfbc1538767cd31193ff77d2fc24
SangYupKim <kylekim95@gmail.com>
2025-04-01 09:34:33 +0900

    feat: test code

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,12 @@
+import RetroButton from '@/components/common/RetroButton';
+
 export default function Home() {
   return (
     <div>
       <p className=" text-center text-5xl p-5">Play ON!</p>
+      <div className="flex justify-center">
+        <RetroButton className="w-[200px] h-[50px] font-suit font-semibold">Hello World!</RetroButton>
+      </div>
     </div>
   );
 }
