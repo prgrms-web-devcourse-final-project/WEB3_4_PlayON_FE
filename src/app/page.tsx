95e9a138abd8d4bd741c74a7a91f7793328d7eff
SangYupKim <kylekim95@gmail.com>
2025-04-01 09:34:33 +0900

    feat: test code

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,9 @@ export default function Home() {
   return (
     <div>
       <p className=" text-center text-5xl p-5">Play ON!</p>
+      <div className="flex justify-center">
+        <RetroButton className="w-[200px] h-[50px] font-suit font-semibold">Hello World!</RetroButton>
+      </div>
     </div>
   );
 }
