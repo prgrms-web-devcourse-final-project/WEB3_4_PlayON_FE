c855323e39b915daed58396bb9baa3d4447c244a
Sang Yup Kim <kylekim95@gmail.com>
2025-04-01 18:01:02 +0900

    15 feat common components (#17)
    3d 느낌 나는 레트로 버튼 제작
    기울이기 기능 구현
    눌렀을때 디자인 변경 기능 구현
    
    type 은 일단 purple 만 존재
    children으로 버튼 안에 들어갈 컨텐츠 채운다
    callback으로 실행할 함수 추가
    className으로 tailwind css 추가 가능

---
+++ src/components/common/RetroButton/index.tsx
@@ -0,0 +1,50 @@
+'use client';
+
+import { MouseEventHandler, ReactNode, useState } from 'react';
+import './style.css';
+
+type RetroButtonProps = {
+  children: ReactNode;
+  type: 'purple';
+  callback?: () => void;
+  className?: string;
+};
+
+export default function RetroButton(props: RetroButtonProps) {
+  const [tiltDirection, setTiltDirection] = useState('');
+  const [active, setActive] = useState('');
+
+  const mouseMoveHandler: MouseEventHandler = (e) => {
+    const bounds = (e.target as HTMLDivElement).getBoundingClientRect();
+    const xPerc = ((e.clientX - bounds.left) / (bounds.right - bounds.left)) * 100;
+    if (xPerc <= 30) {
+      setTiltDirection('hover-left');
+    } else if (xPerc >= 70) {
+      setTiltDirection('hover-right');
+    } else {
+      setTiltDirection('hover');
+    }
+  };
+  const mouseDownHandler: MouseEventHandler = () => {
+    setActive('active');
+  };
+  const mouseUpHandler: MouseEventHandler = () => {
+    if (props.callback) props.callback();
+    setActive('');
+  };
+  return (
+    <div className={`relative ` + props.className}>
+      <div
+        className={`w-full h-full retro-btn ${tiltDirection} ${active} ${props.type} `}
+        onMouseMove={mouseMoveHandler}
+        onMouseLeave={() => setTiltDirection('')}
+        onMouseDown={mouseDownHandler}
+        onMouseUp={mouseUpHandler}
+      >
+        {props.children}
+      </div>
+      <div className={`w-full h-full retro-btn-box-shadow ${tiltDirection} ${active} ${props.type} `}></div>
+      <div className={`w-full h-full retro-btn-shadow ${tiltDirection} ${active} ${props.type} `}></div>
+    </div>
+  );
+}
