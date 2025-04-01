b3235310726e7ef0baad6aaee972f309866f416c
jihye choi <141098707+jihye-c@users.noreply.github.com>
2025-04-01 09:29:07 +0900

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,12 +4,6 @@ export default function Home() {
   return (
     <div>
       <p className=" text-center text-5xl p-5">Play ON!</p>
-      <div className="flex">
-        <PixelCharacter char="mage" motion="attack" />
-        <PixelCharacter char="mage" motion="run" />
-      </div>
-      <PixelCharacter char="archer" motion="run" />
-      <PixelCharacter char="warrior" motion="ulti" />
     </div>
   );
 }
