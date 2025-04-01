7b34302e554f8784a7f0897dcd3e90c3c70f2446
SangYupKim <kylekim95@gmail.com>
2025-04-01 12:08:01 +0900

    style: magic number adjustments

--- src/components/common/RetroButton/index.tsx
+++ src/components/common/RetroButton/index.tsx
@@ -5,11 +5,14 @@ import './style.css';
 
 type RetroButtonProps = {
   children: ReactNode;
+  type: 'purple';
+  callback?: () => void;
   className?: string;
 };
 
 export default function RetroButton(props: RetroButtonProps) {
   const [tiltDirection, setTiltDirection] = useState('');
+  const [active, setActive] = useState('');
 
   const mouseMoveHandler: MouseEventHandler = (e) => {
     const bounds = (e.target as HTMLDivElement).getBoundingClientRect();
@@ -22,13 +25,26 @@ export default function RetroButton(props: RetroButtonProps) {
       setTiltDirection('hover');
     }
   };
+  const mouseDownHandler: MouseEventHandler = () => {
+    setActive('active');
+  };
+  const mouseUpHandler: MouseEventHandler = () => {
+    if (props.callback) props.callback();
+    setActive('');
+  };
   return (
-    <div
-      className={`retro-btn ${tiltDirection} ` + props.className}
-      onMouseMove={mouseMoveHandler}
-      onMouseLeave={() => setTiltDirection('')}
-    >
-      {props.children}
+    <div className="relative">
+      <div
+        className={`retro-btn ${tiltDirection} ${active} ${props.type} ` + props.className}
+        onMouseMove={mouseMoveHandler}
+        onMouseLeave={() => setTiltDirection('')}
+        onMouseDown={mouseDownHandler}
+        onMouseUp={mouseUpHandler}
+      >
+        {props.children}
+      </div>
+      <div className={`retro-btn-box-shadow ${tiltDirection} ${active} ${props.type} ` + props.className}></div>
+      <div className={`retro-btn-shadow ${tiltDirection} ${active} ${props.type} ` + props.className}></div>
     </div>
   );
 }
