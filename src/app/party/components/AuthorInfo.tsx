49f72b9a2c66dcd8d1e224ad1af896801b66a616
marie4053 <marie4053@naver.com>
2025-04-01 16:36:57 +0900

    feat: AuthorInfo 컴포넌트 구현

---
+++ src/app/party/components/AuthorInfo.tsx
@@ -0,0 +1,29 @@
+import { Button } from '@/components/ui/button';
+import { Avatar } from '@radix-ui/react-avatar';
+
+type User = {
+  profile_img: string;
+  title: string;
+  username: string;
+};
+
+interface AuthorInfoProps {
+  data: User;
+}
+
+export default function AuthorInfo({ data }: AuthorInfoProps) {
+  return (
+    <div className="flex gap-6 justify-between items-center w-full">
+      <Avatar
+        style={{
+          backgroundImage: `url(${data.profile_img})`,
+        }}
+        className="bg-cover bg-center size-20 rounded-full shrink-0"
+      />
+      <div className="flex flex-col w-full">
+        <div className="text-base text-neutral-900">{data.title}</div>
+        <div className="text-xl text-neutral-900 font-bold">{data.username}님의 파티</div>
+      </div>
+    </div>
+  );
+}
