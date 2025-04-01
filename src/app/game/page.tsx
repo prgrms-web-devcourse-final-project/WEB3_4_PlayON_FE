c43907335b6c3f21de6f92e42be9bca7f5a0b9f9
TaeHwan Kim <rainkdk02@gmail.com>
2025-04-01 11:28:31 +0900

    feat: add game page

---
+++ src/app/game/page.tsx
@@ -0,0 +1,16 @@
+
+// import PopularCard from "@/components/game/PopularCard";
+// import PickCard from "@/components/game/PickCard";
+// import SteamCard from "@/components/game/SteamCard";
+
+// import Chat from "../Chat";
+// import GuildUser from "../GuildUser";
+// import GuildUserCard from "../GuildUserCard";
+
+export default function Game() {
+
+  return (
+    <>
+    </>
+  );
+}
