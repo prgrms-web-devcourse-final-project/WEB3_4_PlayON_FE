4f6f8efd15c99ed02a9cf58de2cb8c7c7277e662
marie4053 <marie4053@naver.com>
2025-04-01 15:09:34 +0900

    feat: 라디오 버튼 check 여부에 따른 스타일 적용(1/2)

--- src/app/party/components/UserInfoVertical.tsx
+++ src/app/party/components/UserInfoVertical.tsx
@@ -1,3 +1,5 @@
+import { Trophy } from 'lucide-react';
+
 interface UserInfoProps {
   isRadioBtn?: boolean;
   data: User;
@@ -5,15 +7,21 @@ interface UserInfoProps {
 
 export default function UserInfoVertical({ isRadioBtn = false, data }: UserInfoProps) {
   return (
-    <div className="inline-flex items-center gap-2 flex-col">
+    <label htmlFor={data.username} className="inline-flex items-center flex-col gap-2">
+      {isRadioBtn && <input type="radio" id={data.username} name="vipUser" className=" peer" />}
+
       <div
         style={{
           backgroundImage: `url(${data.profile_img})`,
         }}
-        className="rounded-full bg-center bg-cover w-[100px] h-[100px]"
-      />
+        className="relative rounded-full bg-center bg-cover w-[100px] h-[100px] peer-checked:outline-4 peer-checked:outline-yellow-400"
+      >
+        <div className="bg-amber-300 rounded-full p-1 absolute bottom-0 right-0">
+          <Trophy className="size-5 stroke-[1.6px] text-amber-100" />
+        </div>
+      </div>
 
-      <p className="font-suit text-neutral-900 font-bold text-base">{data.username}</p>
-    </div>
+      <p className="font-suit text-neutral-900 font-bold text-base w-[100px] line-clamp-1">{data.username}</p>
+    </label>
   );
 }
