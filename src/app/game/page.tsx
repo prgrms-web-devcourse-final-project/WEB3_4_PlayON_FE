f62e242ea9d0d695bb248482e467773c725a97dc
TaeHwan Kim <rainkdk02@gmail.com>
2025-04-01 12:54:27 +0900

    eat: add game page

---
+++ src/app/game/page.tsx
@@ -0,0 +1,18 @@
+
+// import PopularCard from "@/components/game/PopularCard";
+// import PickCard from "@/components/game/PickCard";
+// import SteamCard from "@/components/game/SteamCard";
+
+// import Chat from "@/components/guildUser/Chat";
+// import GuildUser from "@/components/guildUser/GuildUser";
+// import GuildUserCard from "@/components/guildUser/GuildUserCard";
+
+
+export default function Game() {
+
+  return (
+    <>
+
+    </>
+  );
+}
