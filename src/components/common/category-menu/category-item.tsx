bb2ddc53e85a3014dd566ef6e2b7935c9cbbc919
SangYupKim <kylekim95@gmail.com>
2025-04-01 14:00:56 +0900

    feat: initial

---
+++ src/components/common/category-menu/category-item.tsx
@@ -0,0 +1,7 @@
+// import Toggle from '@/components/ui/toggle';
+
+type CategoryItemProps = { id: string };
+
+export default function CategoryItem(props: CategoryItemProps) {
+  return <div></div>;
+}
