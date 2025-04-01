06d6a410b3794ec56d26ef25e827d48daba71f99
SangYupKim <kylekim95@gmail.com>
2025-04-01 10:12:22 +0900

    fix : need rebase commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,9 @@
-import RetroButton from '@/components/common/RetroButton';
+import PixelCharacter from '@/components/PixelCharacter/PixelCharacter';
 
 export default function Home() {
   return (
     <div>
       <p className=" text-center text-5xl p-5">Play ON!</p>
-      <div className="flex justify-center">
-        <RetroButton className="w-[200px] h-[50px] font-suit font-semibold">Hello World!</RetroButton>
-      </div>
     </div>
   );
 }
