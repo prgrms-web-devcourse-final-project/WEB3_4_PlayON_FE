6bafd8a0ab27a7cc826fe76274dfe769f71358a0
marie4053 <marie4053@naver.com>
2025-04-02 12:14:01 +0900

    design: css 수정

--- src/components/game/SelectedGameCard.tsx
+++ src/components/game/SelectedGameCard.tsx
@@ -11,7 +11,7 @@ export default function SelectedGameCard({ game }: SelectedGameCardProps) {
   return (
     <div
       style={{ backgroundImage: `url(${game.header_image})` }}
-      className="aspect-[410/180] w-full rounded-2xl overflow-hidden"
+      className="aspect-[410/180] rounded-2xl overflow-hidden bg-cover bg-center"
     >
       <div className="bg-gradient-to-r from-black/80 to-black/10 size-full px-8 py-12 place-content-center">
         <div className="text-white font-suit text-base font-normal">선택된 게임</div>
