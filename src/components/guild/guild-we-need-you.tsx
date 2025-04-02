4e5a56b589f08630e9b299a28375215f51e85da1
SangYupKim <kylekim95@gmail.com>
2025-04-02 12:24:42 +0900

    feat : we need you component

---
+++ src/components/guild/guild-we-need-you.tsx
@@ -0,0 +1,50 @@
+'use client';
+
+import guild from '@/types/guild';
+import RetroButton from '../common/RetroButton';
+import { Input } from '@/components/ui/input';
+import { SearchIcon } from 'lucide-react';
+import CapsuleCategoryMenu from '@/components/common/capsule-category-menu';
+
+type WeNeedYouProps = {
+  guildData: guild;
+  className?: string;
+};
+
+export default function WeNeedYou(props: WeNeedYouProps) {
+  return (
+    <div className={`flex flex-col p-8 gap-9 rounded-xl border border-neutral-200 bg-white ${props.className} `}>
+      <div className="flex flex-col gap-5 ">
+        <div className="h-9 bg-slate-400 rounded-xl"></div>
+        <p className="font-dgm line-clamp-1 text-ellipsis overflow-hidden text-neutral-900 text-4xl">
+          {props.guildData.name}
+        </p>
+        <p className="font-suit line-clamp-2 text-ellipsis overflow-hidden text-neutral-900">
+          {props.guildData.description}
+        </p>
+        <div className="flex gap-3">
+          <p className="font-suit text-neutral-900">전체 인원</p>
+          <p className="font-suit font-bold text-neutral-900">{props.guildData.numMembers}명</p>
+        </div>
+      </div>
+      <RetroButton
+        type="purple"
+        callback={() => console.log('!')}
+        className="font-suit text-lg text-white font-semibold w-full h-11"
+      >
+        글쓰기
+      </RetroButton>
+      <svg xmlns="http://www.w3.org/2000/svg" width="347" height="2" viewBox="0 0 347 2" fill="none">
+        <path d="M0 1H347" stroke="#E5E5E5" />
+      </svg>
+      <div className="flex rounded-lg gap-2 text-neutral-400 border border-neutral-300 items-center p-2">
+        <Input
+          placeholder="게시글 제목으로 검색하세요"
+          className="border-none text-sm h-5 focus-visible:ring-transparent"
+        />
+        <SearchIcon className="text-neutral-400" />
+      </div>
+      <CapsuleCategoryMenu items={['공지', '자유', '게임 관련']} multiple={true} />
+    </div>
+  );
+}
