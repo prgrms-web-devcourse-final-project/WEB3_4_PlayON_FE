8e93ac492a76278978255f01abca9f865a3ae153
marie4053 <marie4053@naver.com>
2025-04-02 11:58:28 +0900

    feat: SelectedGameCard 컴포넌트 구현

---
+++ src/components/game/SelectedGameCard.tsx
@@ -0,0 +1,22 @@
+type Game = {
+  name: string;
+  header_image: string;
+};
+
+interface SelectedGameCardProps {
+  game: Game;
+}
+
+export default function SelectedGameCard({ game }: SelectedGameCardProps) {
+  return (
+    <div
+      style={{ backgroundImage: `url(${game.header_image})` }}
+      className="aspect-[410/180] w-full rounded-2xl overflow-hidden"
+    >
+      <div className="bg-gradient-to-r from-black/80 to-black/10 size-full px-8 py-12 place-content-center">
+        <div className="text-white font-suit text-base font-normal">선택된 게임</div>
+        <div className="text-white font-suit text-4xl font-bold">{game.name}</div>
+      </div>
+    </div>
+  );
+}
