a5a34cb6322b389a8f950af5a4a79b14ff16934d
marie4053 <86790183+marie4053@users.noreply.github.com>
2025-04-02 16:06:02 +0900

    feat: selected game card component (#32)
    SelectedGameCard 컴포넌트 작업했습니다.
    
    components > game 폴더 안에 있습니다.
    prop으로 game 데이터만 넘겨주면 됩니다.

---
+++ src/components/game/SelectedGameCard.tsx
@@ -0,0 +1,22 @@
+type Game = {
+  name: string;
+  header_image: string;
+};
+
+interface SelectedGameCardProps {
+  game: Game;
+}
+
+export default function SelectedGameCard({ game }: SelectedGameCardProps) {
+  return (
+    <div
+      style={{ backgroundImage: `url(${game.header_image})` }}
+      className="aspect-[410/180] rounded-2xl overflow-hidden bg-cover bg-center group"
+    >
+      <div className="opacity-0 group-hover:opacity-100 bg-gradient-to-r from-black/80 to-black/10 size-full px-8 py-12 place-content-center transition-all">
+        <div className="text-white font-suit text-base font-normal">선택된 게임</div>
+        <div className="text-white font-suit text-4xl font-bold">{game.name}</div>
+      </div>
+    </div>
+  );
+}
