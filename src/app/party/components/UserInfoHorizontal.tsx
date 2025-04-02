804b486ff54aeb3ecc11ebb8549f9e1bfca2bc84
marie4053 <86790183+marie4053@users.noreply.github.com>
2025-04-02 15:10:25 +0900

    Feat: user info components (#26)
    UserInfoHorizontal 컴포넌트와 UserInfoVertical 컴포넌트 작업했습니다.
    
    1. UserInfoHorizontal
    
    props: size(big | smal), data(유저정보)
    
    2. UserInfoVertical
    
    props: data(유저정보), isRadioBtn, name, onSelected
    
    isRadioBtn: 라디오버튼 유무
    name: 라디오버튼일 경우 input 태그의 name
    onSelected: input값이 변화할 때 실행될 함수(checked 된 input value를 전달하는 역할)

---
+++ src/app/party/components/UserInfoHorizontal.tsx
@@ -0,0 +1,50 @@
+import { cn } from '@/lib/utils';
+
+interface UserInfoProps {
+  size?: 'small' | 'big';
+  data: User;
+}
+
+export default function UserInfo({ size = 'big', data }: UserInfoProps) {
+  return (
+    <div
+      className={cn('inline-flex items-center gap-2', {
+        'gap-[10px]': size === 'small',
+        'items-start': size === 'big',
+      })}
+    >
+      <div
+        style={{
+          backgroundImage: `url(${data.profile_img})`,
+        }}
+        className={cn('rounded-full bg-center bg-cover', {
+          'w-[100px] h-[100px]': size === 'big',
+          'w-9 h-9': size === 'small',
+        })}
+      />
+      <div
+        className={cn('flex gap-1', {
+          'gap-2': size === 'small',
+          'flex-col gap-1': size === 'big',
+        })}
+      >
+        <p
+          className={cn('text-neutral-600 font-suit font-normal', {
+            'text-base': size === 'small',
+            'text-sm': size === 'big',
+          })}
+        >
+          {data.title}
+        </p>
+        <p
+          className={cn('font-suit text-neutral-900 font-bold text-base', {
+            'text-xl': size === 'big',
+            'font-semibold': size === 'small',
+          })}
+        >
+          {data.username}
+        </p>
+      </div>
+    </div>
+  );
+}
---
+++ src/app/party/components/VipRadioGroup.tsx
@@ -0,0 +1,18 @@
+import UserInfoVertical from './UserInfoVertical';
+
+interface VipRadioGroupProps {
+  users: User[];
+  onClick: (value: string) => void;
+}
+
+export default function VipRadioGroup({ users, onClick }: VipRadioGroupProps) {
+  return (
+    <div>
+      <div className="flex gap-4 overflow-x-auto">
+        {users.map((user, idx) => (
+          <UserInfoVertical key={idx} data={user} isRadioBtn name="vip" onSelected={onClick} />
+        ))}
+      </div>
+    </div>
+  );
+}
