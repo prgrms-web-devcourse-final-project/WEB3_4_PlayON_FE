061e93e43252a056f833f2ee3b3e5c2643ea5b9f
TaeHwan Kim <rainkdk02@gmail.com>
2025-04-02 16:17:47 +0900

    Design : Chat component, name 색상 변경 및 design 변경

--- src/components/guildUser/Chat.tsx
+++ src/components/guildUser/Chat.tsx
@@ -1,22 +1,28 @@
 import { guildUser } from '@/types/guildUser';
 import { Avatar } from '../ui/avatar';
 
-type chatprops = {
+type chatProps = {
   data: guildUser;
   isSender: boolean;
   message: string;
 }
 
-export default function Chat({data, isSender, message}: chatprops) {
+export default function Chat(props: chatProps) {
+  const {data, isSender, message} = props
+
   return (
     <>
-      <div className={`flex ${isSender ? 'flex-row-reverse' : 'justify-start flex-row'} gap-5 items-center`}>
+      <div className={`flex ${isSender ? 'justify-start flex-row' : 'flex-row-reverse'} gap-5 items-center`}>
         {<Avatar className="bg-neutral-400 w-16 h-16" />}
 
-        <div className={`box-content bg-white rounded-lg px-5 py-2 ${isSender ? 'mr-3' : 'ml-3'}`}>
-          <div className="flex gap-1 ">
-            <p className="font-suit text-xs text-neutral-400 font-normal">{data.userTitle}</p>
-            <p className="font-suit text-xs text-sky-700 font-bold">{data.name}</p>
+        <div className={`box-content bg-neutral-200 rounded-lg px-5 py-2 ${isSender ? 'mr-3' : 'ml-3'}`}>
+          <div className="flex gap-1">
+            {isSender && (
+              <div>
+                <p className="font-suit text-xs text-neutral-400 font-normal">{data.userTitle}</p>
+                <p className="font-suit text-xs text-purple-600 font-bold">{data.name}</p>
+              </div>
+            )}
           </div>
 
           <p className="font-suit text-base font-normal">{message}</p>
