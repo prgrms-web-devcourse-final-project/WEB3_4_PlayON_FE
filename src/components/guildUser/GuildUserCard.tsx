7fe78fd6ba9ab85cd97f356ec43a1cfe823b1178
TaeHwan Kim <rainkdk02@gmail.com>
2025-04-02 17:01:24 +0900

    feat : guild user badge

--- src/components/guildUser/GuildUserCard.tsx
+++ src/components/guildUser/GuildUserCard.tsx
@@ -3,16 +3,48 @@ import { guildUser } from "@/types/guildUser";
 
 type guildUserCardProps = {
   data: guildUser;
+  memberLevel: string; // leader, manager, user
   // avatarClassName: string;
-}
+};
 
-export default function GuildUserCard({data}: guildUserCardProps) {
-// export default function GuildUserCard({data, avatarClassName="w-full"}: guildUserCardProps) {
+
+
+export default function GuildUserCard(props: guildUserCardProps) {
+  
+  const { data, memberLevel } = props;
+  let badge;
+  
+  switch (memberLevel) {
+    case 'leader':
+      badge = (
+        <div className="bg-amber-300 rounded-full w-5 h-5 absolute bottom-0 right-0">
+          <img src="crown.svg" className="p-1" />
+        </div>
+      );
+      break;
+
+    case 'manager':
+      badge = (
+        <div className="bg-red-300 rounded-full w-5 h-5 absolute bottom-0 right-0">
+          <img src="star.svg" className="p-1" />
+        </div>
+      );
+      break;
+    case 'user':
+      badge = <div></div>;
+  }
+
   return (
     <>
       <div className="box-content rounded-lg border border-neutral-300 px-6 py-8">
         <div className="flex gap-5">
-          <Avatar className="bg-neutral-400 w-16 h-16" />
+          <div className="w-16 h-16 aspect-square relative ">
+            <Avatar className="bg-neutral-400 w-16 h-16" />
+            {badge}
+
+            {/* <div className="bg-amber-300 rounded-full w-5 h-5 absolute bottom-0 right-0">
+            </div> */}
+          </div>
           <div className="">
             {data.userTitle && <p className="font-suit text-sm font-normal text-neutral-500">{data.userTitle}</p>}
             <p className="font-suit text-xl font-medium">{data.name}</p>
