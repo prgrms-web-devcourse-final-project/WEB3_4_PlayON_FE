4c02ce281f63a5f8d6ac14a9fee39fce3a0b60df
SangYupKim <kylekim95@gmail.com>
2025-04-02 09:14:11 +0900

    fix : length bug

--- src/components/common/play-on-rolling-banner.tsx
+++ src/components/common/play-on-rolling-banner.tsx
@@ -3,9 +3,65 @@ import ControllerSVG from '@/components/svg/controller_fill';
 import PacmanSVG from '@/components/svg/pacman_fill';
 import GhostSVG from '@/components/svg/ghost_fill';
 
-export default function PlayOnRollingBanner() {
+export default function PlayOnRollingBanner(props: { className: string }) {
   return (
-    <RollingBanner className="min-w-[1920px] flex-shrink-0 h-16 bg-gradient-to-r from-purple-700 to-purple-500">
+    <RollingBanner
+      className={`min-w-[3840px] flex-shrink-0 h-16 bg-gradient-to-r from-purple-700 to-purple-500 ` + props.className}
+    >
+      <div className="flex items-center w-52">
+        <div className="h-8 mr-5 min-w-11">
+          <ControllerSVG fill="#FFFFFF" stroke="" />
+        </div>
+        <p className="text-white text-3xl font-suit font-black mr-5">PLAYON</p>
+      </div>
+      <div className="flex items-center w-52">
+        <div className="h-8 mr-5 min-w-11">
+          <PacmanSVG fill="#FFFFFF" stroke="" />
+        </div>
+        <p className="text-white text-3xl font-suit font-black mr-5">PLAYON</p>
+      </div>
+      <div className="flex items-center w-52">
+        <div className="h-8 mr-5 min-w-11">
+          <GhostSVG fill="#FFFFFF" stroke="" />
+        </div>
+        <p className="text-white text-3xl font-suit font-black mr-5">PLAYON</p>
+      </div>
+      <div className="flex items-center w-52">
+        <div className="h-8 mr-5 min-w-11">
+          <ControllerSVG fill="#FFFFFF" stroke="" />
+        </div>
+        <p className="text-white text-3xl font-suit font-black mr-5">PLAYON</p>
+      </div>
+      <div className="flex items-center w-52">
+        <div className="h-8 mr-5 min-w-11">
+          <PacmanSVG fill="#FFFFFF" stroke="" />
+        </div>
+        <p className="text-white text-3xl font-suit font-black mr-5">PLAYON</p>
+      </div>
+      <div className="flex items-center w-52">
+        <div className="h-8 mr-5 min-w-11">
+          <GhostSVG fill="#FFFFFF" stroke="" />
+        </div>
+        <p className="text-white text-3xl font-suit font-black mr-5">PLAYON</p>
+      </div>
+      <div className="flex items-center w-52">
+        <div className="h-8 mr-5 min-w-11">
+          <ControllerSVG fill="#FFFFFF" stroke="" />
+        </div>
+        <p className="text-white text-3xl font-suit font-black mr-5">PLAYON</p>
+      </div>
+      <div className="flex items-center w-52">
+        <div className="h-8 mr-5 min-w-11">
+          <PacmanSVG fill="#FFFFFF" stroke="" />
+        </div>
+        <p className="text-white text-3xl font-suit font-black mr-5">PLAYON</p>
+      </div>
+      <div className="flex items-center w-52">
+        <div className="h-8 mr-5 min-w-11">
+          <GhostSVG fill="#FFFFFF" stroke="" />
+        </div>
+        <p className="text-white text-3xl font-suit font-black mr-5">PLAYON</p>
+      </div>
       <div className="flex items-center w-52">
         <div className="h-8 mr-5 min-w-11">
           <ControllerSVG fill="#FFFFFF" stroke="" />
