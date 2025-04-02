222a4b527b6280f6eed862f61e1e110912c7c051
marie4053 <marie4053@naver.com>
2025-04-02 15:34:54 +0900

    design: hover 시에만 overlay 보이게 수정

--- src/components/game/SelectedGameCard.tsx
+++ src/components/game/SelectedGameCard.tsx
@@ -11,9 +11,9 @@ export default function SelectedGameCard({ game }: SelectedGameCardProps) {
   return (
     <div
       style={{ backgroundImage: `url(${game.header_image})` }}
-      className="aspect-[410/180] rounded-2xl overflow-hidden bg-cover bg-center"
+      className="aspect-[410/180] rounded-2xl overflow-hidden bg-cover bg-center group"
     >
-      <div className="bg-gradient-to-r from-black/80 to-black/10 size-full px-8 py-12 place-content-center">
+      <div className="opacity-0 group-hover:opacity-100 bg-gradient-to-r from-black/80 to-black/10 size-full px-8 py-12 place-content-center transition-all">
         <div className="text-white font-suit text-base font-normal">선택된 게임</div>
         <div className="text-white font-suit text-4xl font-bold">{game.name}</div>
       </div>
