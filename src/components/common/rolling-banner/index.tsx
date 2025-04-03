c43f553a43e8406013a45e260aea01088d1c2e73
Sang Yup Kim <kylekim95@gmail.com>
2025-04-03 11:17:38 +0900

    36 feat롤링 배너 스크롤 방향 지정 (#38)
    롤링 배너의 스크롤 방햐을 지정할 수 있게 함
    props로 direction='left' | 'right'

--- src/components/common/rolling-banner/index.tsx
+++ src/components/common/rolling-banner/index.tsx
@@ -1,11 +1,12 @@
-import { CSSProperties, ReactNode } from 'react';
+import { ReactNode } from 'react';
 
 import './style.css';
 
 type RollingBannerProps = {
   className?: string;
   children: ReactNode[];
-  speed: number;
+  duration: number;
+  direction: 'left' | 'right';
 };
 
 export default function RollingBanner(props: RollingBannerProps) {
@@ -13,9 +14,14 @@ export default function RollingBanner(props: RollingBannerProps) {
     return <div className="rolling-item flex items-center justify-center">{child}</div>;
   }
 
+  const scrollDirection = props.direction === 'left' ? 'scrollLeft' : 'scrollRight';
+
   return (
     <div className={`relative flex items-center overflow-hidden ${props.className}`}>
-      <div style={{ animationDuration: `${props.speed}s` }} className={`absolute rolling-list flex items-center gap-5`}>
+      <div
+        style={{ animationDuration: `${props.duration}s`, animationName: `${scrollDirection}` }}
+        className={`absolute rolling-list flex items-center gap-5`}
+      >
         {props.children.map((e) => {
           return RollingBannerItem(e);
         })}
