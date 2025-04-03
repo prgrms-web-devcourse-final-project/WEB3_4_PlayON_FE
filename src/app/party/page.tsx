04d0f31db45be13f9da7432d988f3b048fed4fa3
marie4053 <marie4053@naver.com>
2025-04-03 17:52:37 +0900

    feat: swords icon 추가

--- src/app/party/page.tsx
+++ src/app/party/page.tsx
@@ -5,6 +5,7 @@ import RetroButton from '@/components/common/RetroButton';
 import SearchBar from '@/components/common/SearchBar';
 import PixelCharacter from '@/components/PixelCharacter/PixelCharacter';
 import { gameSimple } from '@/types/games';
+import Image from 'next/image';
 import { useState } from 'react';
 
 const popularGames: gameSimple[] = [
@@ -41,18 +42,23 @@ export default function Party() {
       <section className="wrapper flex justify-between items-end">
         <div>
           <p className="text-neutral-900 text-2xl font-medium">구인중인 파티를 확인하고 빠르게 게임을 시작하세요.</p>
-          <div className="flex gap-6 items-center">
-            <p className="text-purple-700 text-6xl font-extrabold mt-2 mb-6">지금 모집중인 파티</p>
-            <div>아이콘자리</div>
+          <div className="flex gap-6 items-center mt-2 mb-6">
+            <p className="text-purple-700 text-6xl font-extrabold">지금 모집중인 파티</p>
+            <Image
+              src="./img/icons/pixel_swords.svg"
+              alt="swords icon"
+              height={40}
+              style={{ objectFit: 'contain' }}
+            ></Image>
           </div>
           <RetroButton type="purple" className="w-[344px] h-[48px]">
             파티 상세 검색
           </RetroButton>
         </div>
         <div className="flex">
-          {/* <PixelCharacter char="mage" motion="attack" />
+          <PixelCharacter char="mage" motion="attack" />
           <PixelCharacter char="archer" motion="attack" />
-          <PixelCharacter char="warrior" motion="attack" /> */}
+          <PixelCharacter char="warrior" motion="attack" />
         </div>
       </section>
       <PlayOnRollingBanner duration={20} direction="left" />
