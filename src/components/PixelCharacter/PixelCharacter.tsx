bb69ca6505b8f6dc575dbeed34b168e54fb6e2e1
jihye choi <jihyec22@gmail.com>
2025-04-03 19:10:13 +0900

    Refactor: PixelCharacter 수정 컴포넌트 캐릭터간 간격 조절을 위한 className props 추가 캐릭터 묶음에 flex 속성 캐릭터에 className -mx-n 쓰시면 됩니다.

--- src/components/PixelCharacter/PixelCharacter.tsx
+++ src/components/PixelCharacter/PixelCharacter.tsx
@@ -2,8 +2,9 @@ import style from './PixelCharacter.module.css';
 interface Props {
   char: 'archer' | 'mage' | 'warrior';
   motion: 'run' | 'walk' | 'jump' | 'attack' | 'defence' | 'attacked' | 'dying' | 'ulti';
+  className?: string;
 }
-function PixelCharacter({ char, motion }: Props) {
-  return <div className={`w-[144px] h-[96px] ${style[char]} ${style[motion]}`}></div>;
+function PixelCharacter({ char, motion, className }: Props) {
+  return <div className={`w-[144px] h-[96px] ${style[char]} ${style[motion]} ${className}`}></div>;
 }
 export default PixelCharacter;
