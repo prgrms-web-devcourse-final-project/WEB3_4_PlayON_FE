7ec627434e8382a821c4d272d74828ea86a5753a
jihye choi <jihyec22@gmail.com>
2025-04-06 17:44:31 +0900

    Design: 플리커 효과 수정 및 글씨 잘보이도록 수정

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -14,8 +14,9 @@ export default function Login() {
 #####      ################  ##### ######           #########  ######  ######`;
 
   return (
-    <div className="bg-purple-900 text-purple-500 w-full h-full flex flex-col items-center">
+    <div className="bg-purple-900 text-purple-400 w-full h-full flex flex-col items-center">
       <div className="overlay"></div>
+      <div className="scanline"></div>
       <div className="wrapper">
         <div className="mt-16 flex flex-col pb-10">
           <div className="flex gap-5 mb-20">
@@ -27,21 +28,21 @@ export default function Login() {
               <p className="text-2xl font-dgm glow">the game never ends</p>
             </div>
           </div>
-          <div className="font-dgm text-purple-500 mt-5 flex flex-col items-center dashed-border mb-10">
-            <p className="text-4xl text-purple-500 font-dgm bg-purple-900 title">계정으로 로그인</p>
+          <div className="font-dgm text-purple-400 mt-5 flex flex-col items-center dashed-border mb-10">
+            <p className="text-4xl text-purple-400 font-dgm bg-purple-900 title">계정으로 로그인</p>
             <div className="flex flex-col gap-3 pb-8">
               <p className="font-dgm text-2xl">ENTER YOUR EMAIL</p>
-              <Input className="border border-purple-500 rounded-none font-dgm text-lg" />
+              <Input className="border border-purple-500 rounded-none font-dgm !text-xl" />
               <p className="font-dgm text-2xl">ENTER YOUR PASSWORD</p>
-              <Input className="border border-purple-500 rounded-none font-dgm text-lg" />
+              <Input className="border border-purple-500 rounded-none font-dgm !text-xl" />
               <button className="mt-5 flex items-center justify-center gap-2 border border-purple-500 hover:bg-purple-500 hover:text-white p-2">
                 <MailIcon />
                 <span className="font-dgm">LOGIN</span>
               </button>
             </div>
           </div>
-          <div className="font-dgm text-purple-500 mt-5 flex flex-col items-center dashed-border mb-10">
-            <p className="text-4xl text-purple-500 font-dgm bg-purple-900 title">STEAM 인증으로 로그인</p>
+          <div className="font-dgm text-purple-400 mt-5 flex flex-col items-center dashed-border mb-10">
+            <p className="text-4xl text-purple-400 font-dgm bg-purple-900 title">STEAM 인증으로 로그인</p>
             <div className="p-2 pb-8">
               <button className="p-2 flex items-center justify-center gap-2 rounded hover:outline outline-purple-500">
                 <SteamSVG fill={'#8258ff'} stroke="" width={48} height={48} />
