95a3e4002d72e7a9c1db25510edb754f526698ac
SangYupKim <kylekim95@gmail.com>
2025-04-06 20:17:07 +0900

    feat : 길드 상세 페이지

--- src/app/guild/[guildid]/page.tsx
+++ src/app/guild/[guildid]/page.tsx
@@ -1,18 +1,25 @@
 'use client';
 
-import { dummyGuild } from '@/utils/dummyData';
+import { dummyGuild, dummyPost, dummyGuildUser } from '@/utils/dummyData';
 import Tag from '@/components/common/Tag';
 import { Button } from '@/components/ui/button';
-import { ClipboardPenIcon } from 'lucide-react';
+import { ChevronRight, ClipboardPenIcon } from 'lucide-react';
 import RetroButton from '@/components/common/RetroButton';
 import PlayOnRollingBanner from '@/components/common/play-on-rolling-banner';
+import CommunityPostImageLong from '@/components/community/post-image-long';
+import CommunityPostLong from '@/components/community/post-long';
+import CommunityPostImageShort from '@/components/community/post-image-short';
+import CommunityPostShort from '@/components/community/post-short';
+import GuildUserCard from '@/components/guildUser/GuildUserCard';
+import { guildUser } from '@/types/guild';
 
 export default function GuildDetails() {
   const allTags = [...dummyGuild.friendly, ...dummyGuild.gender, ...dummyGuild.play_style, ...dummyGuild.skill_level];
+  const dummyGuildUsers = new Array<guildUser>(9).fill(dummyGuildUser);
 
   return (
-    <div className="flex flex-col mt-[100px]">
-      <div className="flex gap-6 w-[67%] mb-16 self-center">
+    <div className="flex flex-col mt-36 mb-36 gap-14">
+      <div className="flex gap-6 w-[67%] self-center">
         <div className="flex flex-col gap-4">
           <img src={dummyGuild.img_src} alt="" className="w-[50%] min-w-[628px] rounded-3xl" />
           <Button variant="outline" className="w-fit px-4 py-2 text-neutral-500">
@@ -50,9 +57,40 @@ export default function GuildDetails() {
         </div>
       </div>
       <PlayOnRollingBanner direction="right" duration={20} />
-      <div className="flex flex-col">
-        <div className="flex">
-          <p className="text-4xl font-bold text-neutral-900 w-[67%] self-center">공지</p>
+      <div className="flex flex-col w-[67%] self-center">
+        <div className="flex w-full justify-between">
+          <p className="text-4xl font-bold text-neutral-900">공지</p>
+          <div className="flex items-center">
+            <p className="text-xl font-medium text-neutral-900">전체 보기</p>
+            <ChevronRight />
+          </div>
+        </div>
+        <div className="flex flex-col">
+          <CommunityPostImageLong data={dummyPost} className="h-44 border-b border-neutral-400" />
+          <CommunityPostLong data={dummyPost} className="h-44" />
+        </div>
+      </div>
+      <div className="flex flex-col w-[67%] self-center gap-8">
+        <p className="text-4xl font-bold text-neutral-900">멤버</p>
+        <div className="grid grid-cols-3 gap-x-6 gap-y-6">
+          {dummyGuildUsers.map((e, ind) => (
+            <GuildUserCard key={`${e.user.username}_${ind}`} data={e} />
+          ))}
+        </div>
+      </div>
+      <div className="flex flex-col gap-6 w-[67%] self-center">
+        <div className="flex w-full justify-between">
+          <p className="text-4xl font-bold text-neutral-900">길드 최신글</p>
+          <div className="flex items-center">
+            <p className="text-xl font-medium text-neutral-900">전체 보기</p>
+            <ChevronRight />
+          </div>
+        </div>
+        <div className="grid grid-cols-2 gap-x-6 gap-y-6">
+          <CommunityPostShort data={dummyPost} className="h-52" />
+          <CommunityPostImageShort data={dummyPost} className="h-52" />
+          <CommunityPostImageShort data={dummyPost} className="h-52" />
+          <CommunityPostShort data={dummyPost} className="h-52" />
         </div>
       </div>
     </div>
