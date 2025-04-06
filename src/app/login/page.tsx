1930a35ceb3e943d3f5272aa0c6c5febc11f6872
SangYupKim <kylekim95@gmail.com>
2025-04-06 17:11:54 +0900

    feat 로그인 페이지

---
+++ src/app/login/page.tsx
@@ -0,0 +1,62 @@
+import SteamSVG from '@/components/svg/steam';
+import './style.css';
+import { Input } from '@/components/ui/input';
+import { MailIcon } from 'lucide-react';
+import Link from 'next/link';
+
+export default function Login() {
+  const playOnASCII = `##########  ######      ####### ######  ######       #######    ####### #####
+##################      #######%#############      ###########% ####### #####
+#####%###########      #########  #########      ############################
+########### #####      ##########  #######       ######   ###################
+##########  #####%###%############ ######        ####### ####################
+#####       ###################### ######         ################### #######
+#####      ################  ##### ######           #########  ######  ######`;
+
+  return (
+    <div className="bg-purple-900 text-purple-500 w-full h-full flex flex-col items-center">
+      <div className="overlay"></div>
+      <div className="wrapper">
+        <div className="mt-16 flex flex-col pb-10">
+          <div className="flex gap-5 mb-20">
+            <div className="">
+              <pre className="text-xs glow">{playOnASCII}</pre>
+            </div>
+            <div>
+              <p className="text-2xl font-dgm glow">when we play on together</p>
+              <p className="text-2xl font-dgm glow">the game never ends</p>
+            </div>
+          </div>
+          <div className="font-dgm text-purple-500 mt-5 flex flex-col items-center dashed-border mb-10">
+            <p className="text-4xl text-purple-500 font-dgm bg-purple-900 title">계정으로 로그인</p>
+            <div className="flex flex-col gap-3 pb-8">
+              <p className="font-dgm text-2xl">ENTER YOUR EMAIL</p>
+              <Input className="border border-purple-500 rounded-none font-dgm text-lg" />
+              <p className="font-dgm text-2xl">ENTER YOUR PASSWORD</p>
+              <Input className="border border-purple-500 rounded-none font-dgm text-lg" />
+              <button className="mt-5 flex items-center justify-center gap-2 border border-purple-500 hover:bg-purple-500 hover:text-white p-2">
+                <MailIcon />
+                <span className="font-dgm">LOGIN</span>
+              </button>
+            </div>
+          </div>
+          <div className="font-dgm text-purple-500 mt-5 flex flex-col items-center dashed-border mb-10">
+            <p className="text-4xl text-purple-500 font-dgm bg-purple-900 title">STEAM 인증으로 로그인</p>
+            <div className="p-2 pb-8">
+              <button className="p-2 flex items-center justify-center gap-2 rounded hover:outline outline-purple-500">
+                <SteamSVG fill={'#8258ff'} stroke="" width={48} height={48} />
+                <p className="text-4xl font-black">STEAM</p>
+              </button>
+            </div>
+          </div>
+          <div className="flex flex-col items-center">
+            <p className="font-dgm text-2xl glow">아이디가 없나요?</p>
+            <Link href="/signup">
+              <p className="font-dgm text-2xl glow">{`[ 회원 가입하러 가기 ]`}</p>
+            </Link>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
