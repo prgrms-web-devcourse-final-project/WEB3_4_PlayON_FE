dfc7b5d3a1801d2460eb8024fd67eb469072b8d6
marie4053 <marie4053@naver.com>
2025-04-06 11:23:17 +0900

    feat: 컴포넌트 수정 - skeleton 추가 - data 없을 때 디자인 추가

--- src/components/game/SelectedGameCard.tsx
+++ src/components/game/SelectedGameCard.tsx
@@ -1,10 +1,26 @@
 import { gameSimple } from '@/types/games';
+import { Skeleton } from '../ui/skeleton';
+import { Gamepad2 } from 'lucide-react';
 
 interface SelectedGameCardProps {
-  data: gameSimple;
+  data?: gameSimple;
+}
+
+export function SelectedGameCardSkeleton() {
+  return <Skeleton className="aspect-[410/180] rounded-2xl" />;
 }
 
 export default function SelectedGameCard({ data }: SelectedGameCardProps) {
+  if (!data) {
+    return (
+      <div className="aspect-[410/180] bg-neutral-100 rounded-2xl flex justify-center items-center">
+        <div className="flex flex-col gap-2 items-center">
+          <Gamepad2 size={40} strokeWidth={1.4} className="text-neutral-400" />
+          <p className="text-neutral-400 text-base font-semibold">제한된 게임이 없습니다.</p>
+        </div>
+      </div>
+    );
+  }
   return (
     <div
       style={{ backgroundImage: `url(${data.img_src})` }}
