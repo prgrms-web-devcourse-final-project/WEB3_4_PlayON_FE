5d6523b8c52c7f36420286b5c7f76a89028ab1c0
jihye choi <jihyec22@gmail.com>
2025-04-06 20:28:42 +0900

    Feat: formatDate 함수 fullDate 타입 추가

--- src/utils/formatDate.ts
+++ src/utils/formatDate.ts
@@ -1,5 +1,15 @@
 // 날짜 포맷팅 함수
-export default function formatDate(targetDate: Date) {
+export default function formatDate(targetDate: Date, fullDate?: boolean) {
+  if (fullDate) {
+    return targetDate.toLocaleString('ko-KR', {
+      year: 'numeric',
+      month: 'long',
+      day: 'numeric',
+      hour: '2-digit',
+      minute: '2-digit',
+      hour12: false,
+    });
+  }
   return targetDate.toLocaleString('ko-KR', {
     month: 'long',
     day: 'numeric',
