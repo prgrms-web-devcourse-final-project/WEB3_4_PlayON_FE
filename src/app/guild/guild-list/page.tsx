352ba7ebf2c257b80b5d9a80adc3155222f207f6
marie4053 <marie4053@naver.com>
2025-04-06 10:31:37 +0900

    feat: guild-list page ui 작업

---
+++ src/app/guild/guild-list/page.tsx
@@ -0,0 +1,82 @@
+'use client';
+import HeroSwiperBanner from '@/components/common/HeroSwiperBanner';
+import SortRadioGroup, { SortOption } from '@/components/common/SortRadioGroup';
+import GuildHorizon from '@/components/guild/guild-horizon';
+import {
+  Pagination,
+  PaginationContent,
+  PaginationEllipsis,
+  PaginationItem,
+  PaginationLink,
+  PaginationNext,
+  PaginationPrevious,
+} from '@/components/ui/pagination';
+import { guild } from '@/types/guild';
+import { dummyGuild } from '@/utils/dummyData';
+import GuildSearchComponent from '@/components/guild/guild-search-component';
+import { ChevronDown } from 'lucide-react';
+// import Link from 'next/link';
+import { useState } from 'react';
+
+const sortOptions: SortOption[] = [
+  { id: 'latest', label: '최신순' },
+  { id: 'activity', label: '활동순' },
+  { id: 'members', label: '인원순' },
+];
+
+const imageList = ['/img/hero/bg_guild_1.webp', '/img/hero/bg_guild_2.webp', '/img/hero/bg_guild_3.webp'];
+
+export default function GuildList() {
+  const [selectedSort, setSelectedSort] = useState(sortOptions[0].id);
+
+  const dummyGuildList: guild[] = Array(9).fill(dummyGuild);
+
+  return (
+    <div className="space-y-16 mb-24">
+      <section className="w-full h-[520px]">
+        <HeroSwiperBanner imageList={imageList}>
+          <div className="wrapper">
+            <p className="font-[helvetica] text-9xl text-white font-extrabold absolute -bottom-6 ">GUILD LIST</p>
+          </div>
+        </HeroSwiperBanner>
+      </section>
+      <section className="wrapper group space-y-6 min-h-16">
+        <label className="flex gap-8 items-center">
+          <input type="checkbox" className="peer hidden" />
+          <p className="text-4xl font-extrabold text-neutral-900">필터</p>
+          <ChevronDown size={32} className="peer-checked:rotate-180 transition-transform" />
+        </label>
+        <div>
+          {/* <div className="bg-neutral-200 rounded-lg w-full h-[400px] group-has-[input:checked]:h-0 group-has-[input:checked]:opacity-0 transition-all duration-500 ease-in-out"></div> */}
+          <GuildSearchComponent className="w-full" />
+        </div>
+      </section>
+      <section className="wrapper space-y-10">
+        <SortRadioGroup options={sortOptions} value={selectedSort} onChange={setSelectedSort} />
+        <div className="grid grid-cols-3 gap-6">
+          {dummyGuildList.map((guild) => (
+            <GuildHorizon key={guild.guild_name} data={guild} />
+          ))}
+        </div>
+        <Pagination>
+          <PaginationContent>
+            <PaginationItem>
+              <PaginationPrevious href="#" className="text0" />
+            </PaginationItem>
+            <PaginationItem>
+              <PaginationLink href="#" isActive>
+                1
+              </PaginationLink>
+            </PaginationItem>
+            <PaginationItem>
+              <PaginationEllipsis />
+            </PaginationItem>
+            <PaginationItem>
+              <PaginationNext href="#" />
+            </PaginationItem>
+          </PaginationContent>
+        </Pagination>
+      </section>
+    </div>
+  );
+}
