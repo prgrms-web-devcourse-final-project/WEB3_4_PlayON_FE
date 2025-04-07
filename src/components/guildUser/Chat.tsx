6db86da59f2d71011746fbeac604cf1741e6651e
jihye choi <jihyec22@gmail.com>
2025-04-07 17:01:23 +0900

    Refactor: 채팅 컴포넌트 수정

--- src/components/guildUser/Chat.tsx
+++ src/components/guildUser/Chat.tsx
@@ -1,8 +1,9 @@
+import { userSimple } from '@/types/user';
 import { Avatar, AvatarImage } from '../ui/avatar';
 import { guildUser } from '@/types/guild';
 
 interface chatCradProps {
-  data: guildUser;
+  data: userSimple;
   isSender?: boolean;
   message: string;
 }
@@ -12,16 +13,18 @@ export default function Chat(props: chatCradProps) {
 
   return (
     <>
-      <div className={`flex ${isSender ? 'justify-start flex-row' : 'flex-row-reverse'} gap-5 items-center`}>
-        <Avatar className="bg-neutral-400 w-16 h-16">
-          <AvatarImage src={data.user.img_src} />
-        </Avatar>
+      <div className={`flex ${!isSender ? 'justify-start flex-row' : 'flex-row-reverse'} gap-2 items-center`}>
+        {!isSender && (
+          <Avatar className="bg-neutral-400 w-16 h-16">
+            <AvatarImage src={data.img_src} />
+          </Avatar>
+        )}
         <div className={`box-content bg-neutral-200 rounded-lg px-5 py-2 ${isSender ? 'mr-3' : 'ml-3'}`}>
           <div className="flex gap-1">
-            {isSender && (
+            {!isSender && (
               <div>
-                <p className="font-suit text-xs text-neutral-400 font-normal">{data.user.user_title}</p>
-                <p className="font-suit text-xs text-purple-600 font-bold">{data.user.nickname}</p>
+                <p className="font-suit text-xs text-neutral-400 font-normal">{data.user_title}</p>
+                <p className="font-suit text-xs text-purple-600 font-bold">{data.nickname}</p>
               </div>
             )}
           </div>
