bc9d002d218f47d35985d1079895e8cb37554924
TaeHwan Kim <rainkdk02@gmail.com>
2025-04-07 17:39:51 +0900

    design: 회원 정보 수정 모달 디자인

--- src/app/user/me/components/MyModal.tsx
+++ src/app/user/me/components/MyModal.tsx
@@ -3,21 +3,13 @@
 import SteamSVG from '@/components/svg/steam';
 import { Avatar, AvatarImage } from '@/components/ui/avatar';
 import { Button } from '@/components/ui/button';
-import {
-  Dialog,
-  DialogContent,
-  DialogFooter,
-  DialogHeader,
-  DialogTrigger,
-} from '@/components/ui/dialog';
+import { Dialog, DialogContent, DialogFooter, DialogHeader, DialogTrigger } from '@/components/ui/dialog';
 import { Input } from '@/components/ui/input';
 import { Label } from '@/components/ui/label';
 import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from '@/components/ui/select';
 import { dummyUserDetail, dummyUserSimple } from '@/utils/dummyData';
 import { ImageUp, SquarePen } from 'lucide-react';
-import {  useRef } from 'react';
-
-
+import { useRef } from 'react';
 
 export function EditInfo() {
   // if (!isOpen) return null;
@@ -31,7 +23,7 @@ export function EditInfo() {
     }
   };
 
-  const dummyTitle = ['나한테 바나나?', '달인', '넌 네거야!', '묵찌빠 달인', '매너온도 200', '']
+  const dummyTitle = ['나한테 바나나?', '달인', '넌 네거야!', '묵찌빠 달인', '매너온도 200', ''];
 
   return (
     <Dialog>
@@ -44,7 +36,6 @@ export function EditInfo() {
         <DialogHeader>
           <p>회원정보 수정</p>
         </DialogHeader>
-
 
         <div className="flex gap-7">
           <div className="relative">
@@ -61,38 +52,44 @@ export function EditInfo() {
           </div>
 
           <div className="flex flex-col gap-5">
-            <div className="flex gap-3">
-              <Select>
-                <SelectTrigger className="font-suit text-2xl font-semibold text-neutral-400" id="user_title">
-                  <SelectValue
-                    className="font-suit text-2xl font-semibold text-neutral-400"
-                    placeholder={dummyUserSimple.user_title}
-                  />
-                </SelectTrigger>
-                <SelectContent position="popper">
-                  <SelectItem value="user_title">AdventureTime!</SelectItem>
-                  {dummyTitle.map((title, index) => (
-                      <SelectItem key={index} value={"usertitle"[index]}>{title}</SelectItem>
-                  ))}
-                  {/* <SelectItem value="next">Next.js</SelectItem>
+            <div className='flex flex-row gap-2'>
+              <div className="flex gap-3">
+                <Select>
+                  <SelectTrigger className="font-suit text-2xl font-semibold text-neutral-400" id="user_title">
+                    <SelectValue
+                      className="font-suit text-2xl font-semibold text-neutral-400"
+                      placeholder={dummyUserSimple.user_title}
+                    />
+                  </SelectTrigger>
+                  <SelectContent position="popper">
+                    <SelectItem value="user_title">AdventureTime!</SelectItem>
+                    {dummyTitle.map((title, index) => (
+                      <SelectItem key={index} value={'usertitle'[index]}>
+                        {title}
+                      </SelectItem>
+                    ))}
+                    {/* <SelectItem value="next">Next.js</SelectItem>
                   <SelectItem value="sveltekit">SvelteKit</SelectItem>
                   <SelectItem value="astro">Astro</SelectItem>
                   <SelectItem value="nuxt">Nuxt.js</SelectItem> */}
-                </SelectContent>
-              </Select>
+                  </SelectContent>
+                </Select>
+              </div>
               <p className="font-suit text-2xl font-bold ">{dummyUserSimple.nickname}</p>
             </div>
 
             <div className="flex gap-8">
               <div className="font-suit text-base font-semibold text-neutral-400 flex">
                 스팀 아이디 :&nbsp;
-                {isSteamToken ? <div>{dummyUserSimple.username.split('@')[0]}</div> :
+                {isSteamToken ? (
+                  <div>{dummyUserSimple.username.split('@')[0]}</div>
+                ) : (
                   // <button>Steam ID 연동</button>
-                                          <button className='flex flex-row gap-1'>
-                                            <SteamSVG fill={'#8258ff'} stroke="" width={24} height={24} />
-                                            <p className="text-base font-black text-purple-400">STEAM</p>
-                                          </button>
-                }
+                  <button className="flex flex-row gap-1">
+                    <SteamSVG fill={'#8258ff'} stroke="" width={24} height={24} />
+                    <p className="text-base font-black text-purple-400">STEAM</p>
+                  </button>
+                )}
               </div>
 
               <div className="flex relative">
@@ -104,15 +101,9 @@ export function EditInfo() {
                       <SelectValue placeholder={dummyUserDetail.gender} />
                     </SelectTrigger>
                     <SelectContent position="popper">
-                      <SelectItem value="next">
-                        남자
-                      </SelectItem>
-                      <SelectItem value="sveltekit">
-                        여자
-                      </SelectItem>
-                      <SelectItem value="user_title">
-                        None
-                      </SelectItem>
+                      <SelectItem value="next">남자</SelectItem>
+                      <SelectItem value="sveltekit">여자</SelectItem>
+                      <SelectItem value="user_title">None</SelectItem>
                     </SelectContent>
                   </Select>
                 </div>
@@ -128,9 +119,7 @@ export function EditInfo() {
             </div>
 
             <div className="flex items-center gap-3 w-[310px]">
-              <Label htmlFor="password">
-                비밀번호
-              </Label>
+              <Label htmlFor="password">비밀번호</Label>
               <Input id="password" type="password" />
             </div>
           </div>
