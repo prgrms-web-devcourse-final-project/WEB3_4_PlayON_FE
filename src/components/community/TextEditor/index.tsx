29cf3b40313e88e99bfd9e1e3c3bc47a4dc136d5
marie4053 <marie4053@naver.com>
2025-04-07 11:29:35 +0900

    feat: TextEditor 컴포넌트 생성

---
+++ src/components/community/TextEditor/index.tsx
@@ -0,0 +1,44 @@
+'use client';
+
+import { useMemo } from 'react';
+import dynamic from 'next/dynamic';
+import 'react-quill-new/dist/quill.snow.css';
+import { cn } from '@/lib/utils';
+import './style.css';
+
+// 서버에서는 실행되지 않도록 dynamic import
+const ReactQuill = dynamic(() => import('react-quill-new'), { ssr: false });
+
+interface TextEditorProps {
+  value: string;
+  onChange: (value: string) => void;
+  className?: string;
+}
+
+export default function TextEditor({ value, onChange, className }: TextEditorProps) {
+  // 리렌더링 될 때마다 옵션이 새로 생성되는 것을 막기 위함
+  const modules = useMemo(
+    () => ({
+      toolbar: [
+        [{ header: [1, 2, 3, 4, false] }],
+        [{ list: 'ordered' }, { list: 'bullet' }],
+        ['bold', 'italic', 'underline', 'strike', 'blockquote'],
+        [{ color: [] }, { background: [] }],
+        [{ align: [] }],
+        ['link', 'image'],
+      ],
+    }),
+    []
+  );
+
+  return (
+    <ReactQuill
+      theme="snow"
+      value={value}
+      onChange={onChange}
+      modules={modules}
+      placeholder="내용을 입력하세요"
+      className={className}
+    />
+  );
+}
