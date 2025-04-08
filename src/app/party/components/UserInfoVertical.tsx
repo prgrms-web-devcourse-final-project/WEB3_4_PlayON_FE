5ba835b6f9c6611bad7773d1bf7687482f332018
marie4053 <86790183+marie4053@users.noreply.github.com>
2025-04-08 12:30:05 +0900

    design: UserInfoVertical css 수정 (#119)
    UserInfoVertical css 수정 이미지 크기 유지

--- src/app/party/components/UserInfoVertical.tsx
+++ src/app/party/components/UserInfoVertical.tsx
@@ -40,15 +40,15 @@ export default function UserInfoVertical({ isRadioBtn = false, data, name, onSel
           style={{
             background: 'linear-gradient(-27deg, #FCD34D 0%, #FFECAF 29%, #EFAE45 100%)',
           }}
-          className="opacity-0 peer-checked:opacity-100 rounded-full bg-amber-300 size-[100px] transition-all ease-linear duration-100"
+          className="absolute opacity-0 -z-10 -top-1 -left-[5px] peer-checked:opacity-100 rounded-full bg-amber-300 size-[108px] transition-all ease-linear duration-100"
         ></div>
         <div
           style={{
             backgroundImage: `url(${data.img_src})`,
           }}
-          className="absolute top-0 rounded-full bg-center bg-cover w-[100px] h-[100px] peer-checked:size-[92px] peer-checked:top-1 peer-checked:left-1 peer-checked:shadow-[inset_0px_0px_12px_3px_rgba(0,0,0,0.32)]"
+          className="rounded-full bg-center bg-cover size-[100px] peer-checked:shadow-[inset_0px_0px_12px_3px_rgba(0,0,0,0.32)] place-self-center"
         />
-        <div className="opacity-0 peer-checked:opacity-20 absolute top-1 left-1 size-[92px] rounded-full bg-[#D18800]" />
+        <div className="opacity-0 peer-checked:opacity-20 size-[100px] rounded-full bg-[#D18800] absolute top-0" />
 
         <div className="opacity-0 peer-checked:opacity-100 bg-amber-300 rounded-full p-1 absolute bottom-0 right-0 transition-all ease-linear duration-100">
           <Trophy className="size-5 stroke-[1.6px] text-amber-100" />
