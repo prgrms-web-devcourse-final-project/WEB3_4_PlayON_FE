0afeb3eedba02a4831686ab65d552e6372ecda50
SangYupKim <kylekim95@gmail.com>
2025-04-08 11:17:20 +0900

    feat : cooler category menu

--- src/app/signup/initial/page.tsx
+++ src/app/signup/initial/page.tsx
@@ -1,8 +1,24 @@
+'use client';
+
 import SteamSVG from '@/components/svg/steam';
 import './style.css';
 import { Input } from '@/components/ui/input';
 import { MailIcon } from 'lucide-react';
 import Link from 'next/link';
+import { z } from 'zod';
+import { userSchema } from '../userSchema';
+import { useForm } from 'react-hook-form';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { Form, FormControl, FormField, FormItem } from '@/components/ui/form';
+import { useRouter } from 'next/navigation';
+import { useState } from 'react';
+
+const userInitialSchema = userSchema.pick({
+  email: true,
+  password: true,
+  nickname: true,
+});
+type UserInitialSchema = z.infer<typeof userInitialSchema>;
 
 export default function SignupInitial() {
   const playOnASCII = `##########  ######      ####### ######  ######       #######    ####### #####
@@ -12,6 +28,21 @@ export default function SignupInitial() {
 ##########  #####%###%############ ######        ####### ####################
 #####       ###################### ######         ################### #######
 #####      ################  ##### ######           #########  ######  ######`;
+  const form = useForm<UserInitialSchema>({
+    resolver: zodResolver(userInitialSchema),
+    defaultValues: {
+      email: '',
+      password: '',
+      nickname: '',
+    },
+  });
+  const router = useRouter();
+
+  function onSubmit(data: UserInitialSchema) {
+    console.log(data);
+    router.push('/signup/userdata');
+  }
+  const [submitHover, setSubmitHover] = useState(false);
 
   return (
     <div className="bg-purple-900 text-purple-400 w-full h-screen flex flex-col items-center">
@@ -31,22 +62,79 @@ export default function SignupInitial() {
           <div className="font-dgm text-purple-400 mt-5 flex flex-col items-center dashed-border mb-10">
             <p className="text-4xl text-purple-400 font-dgm bg-purple-900 title">이메일로 회원가입</p>
             <div className="flex flex-col gap-3 pb-8">
-              <p className="font-dgm text-2xl">ENTER YOUR EMAIL</p>
-              <Input className="border border-purple-500 rounded-none font-dgm !text-xl" />
-              <p className="font-dgm text-2xl">ENTER YOUR PASSWORD</p>
-              <Input type="password" className="border border-purple-500 rounded-none font-dgm !text-xl" />
-              <button className="mt-5 flex items-center justify-center gap-2 border border-purple-500 hover:bg-purple-500 hover:text-white p-2">
-                <MailIcon />
-                <span className="font-dgm">REGISTER</span>
-              </button>
+              <Form {...form}>
+                <form onSubmit={form.handleSubmit(onSubmit)}>
+                  <p className="font-dgm text-2xl glow">ENTER YOUR EMAIL</p>
+                  <FormField
+                    control={form.control}
+                    name="email"
+                    render={({ field }) => (
+                      <FormItem>
+                        <FormControl>
+                          <Input
+                            type="email"
+                            className="border border-purple-500 rounded-none font-dgm !text-xl"
+                            value={field.value}
+                            onChange={field.onChange}
+                          />
+                        </FormControl>
+                      </FormItem>
+                    )}
+                  />
+                  <p className="font-dgm text-2xl glow">ENTER YOUR PASSWORD</p>
+                  <FormField
+                    control={form.control}
+                    name="password"
+                    render={({ field }) => (
+                      <FormItem>
+                        <FormControl>
+                          <Input
+                            type="password"
+                            className="border border-purple-500 rounded-none font-dgm !text-xl"
+                            value={field.value}
+                            onChange={field.onChange}
+                          />
+                        </FormControl>
+                      </FormItem>
+                    )}
+                  />
+                  <p className="text-purple-500 font-dgm text-2xl glow">ENTER YOUR NICKNAME</p>
+                  <FormField
+                    control={form.control}
+                    name="nickname"
+                    render={({ field }) => (
+                      <FormItem>
+                        <FormControl>
+                          <Input
+                            className="border border-purple-500 rounded-none font-dgm !text-xl"
+                            value={field.value}
+                            onChange={field.onChange}
+                          />
+                        </FormControl>
+                      </FormItem>
+                    )}
+                  />
+                  <button
+                    type="submit"
+                    className="justify-self-center mt-5 flex items-center justify-center gap-2 border border-purple-500 hover:bg-purple-500 hover:text-white py-2 px-5"
+                  >
+                    <MailIcon />
+                    <span className="font-dgm">REGISTER</span>
+                  </button>
+                </form>
+              </Form>
             </div>
           </div>
           <div className="font-dgm text-purple-400 mt-5 flex flex-col items-center dashed-border mb-10">
             <p className="text-4xl text-purple-400 font-dgm bg-purple-900 title">STEAM으로 회원가입</p>
             <div className="p-2 pb-8">
-              <button className="p-2 flex items-center justify-center gap-2 rounded hover:outline outline-purple-500">
-                <SteamSVG fill={'#8258ff'} stroke="" width={48} height={48} />
-                <p className="text-4xl font-black">STEAM</p>
+              <button
+                className="p-2 flex items-center justify-center gap-2"
+                onMouseEnter={() => setSubmitHover(true)}
+                onMouseLeave={() => setSubmitHover(false)}
+              >
+                <SteamSVG fill={`${submitHover ? '#bdb9f6' : '#8258ff'}`} stroke="" width={48} height={48} />
+                <p className={`text-4xl font-black ${submitHover ? 'text-purple-200 glow' : ''}`}>STEAM</p>
               </button>
             </div>
           </div>
