00b08bf3b45e1955f15b9e4e0b16de1d56134f52
marie4053 <marie4053@naver.com>
2025-04-08 20:47:19 +0900

    feat: PATH에 라우트 추가

--- src/constants/routes/index.ts
+++ src/constants/routes/index.ts
@@ -1,7 +1,14 @@
-import { RouteType } from './route';
+import { COMMUNITY_ROUTE } from './community';
+import { GAME_ROUTE } from './game';
+import { GUILD_ROUTE } from './guild';
+import { PARTY_ROUTE } from './party';
 import { USER_ROUTE } from './user';
 
-export const PATH: RouteType = Object.freeze({
+export const PATH = Object.freeze({
   main: '/',
   ...USER_ROUTE,
+  ...PARTY_ROUTE,
+  ...GUILD_ROUTE,
+  ...GAME_ROUTE,
+  ...COMMUNITY_ROUTE,
 });
