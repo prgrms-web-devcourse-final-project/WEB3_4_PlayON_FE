826eb5562761c7eff47dcc9024deb80c1c282e8a
marie4053 <marie4053@naver.com>
2025-04-08 15:04:34 +0900

    design: InputImage 컴포넌트 수정

--- src/components/community/input-image.tsx
+++ src/components/community/input-image.tsx
@@ -10,16 +10,24 @@ interface InputImageProps {
 export default function InputImage({ onChange, previewUrl }: InputImageProps) {
   return (
     <>
-      <Label>
-        <Input type="file" id="image" onChange={onChange} className="hidden" />
-        <div className="flex w-full h-[180px] rounded-xl border border-neutral-300 bg-white cursor-pointer justify-center items-center overflow-hidden">
+      <div className="flex gap-4">
+        <Label
+          htmlFor="image"
+          className="flex aspect-[16/9] min-w-[400px] max-w-[400px] rounded-xl border border-neutral-200 bg-white cursor-pointer justify-center items-center overflow-hidden"
+        >
           {previewUrl ? (
             <img src={previewUrl} alt="미리보기" className="object-cover w-full h-full" />
           ) : (
-            <Image strokeWidth={1.6} className="text-neutral-300 size-10" />
+            <Image strokeWidth={1.6} className="text-neutral-300 size-10 place-self-center" />
           )}
-        </div>
-      </Label>
+        </Label>
+        <Input
+          type="file"
+          id="image"
+          onChange={onChange}
+          className="text-neutral-500 font-normal file:text-neutral-700 w-[400px] bg-white"
+        />
+      </div>
     </>
   );
 }
