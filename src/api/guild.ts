a95b1a13067e0a3de7b125b6b9eea1fe64e89dc4
marie4053 <marie4053@naver.com>
2025-04-09 21:17:04 +0900

    feat: GetGuild 함수 완성

--- src/api/guild.ts
+++ src/api/guild.ts
@@ -1,7 +1,8 @@
 import { GUILD_ENDPOINTS as GUILD } from '@/constants/endpoints/guild';
 import { useAxios } from '@/hooks/useAxios';
+import { guild } from '@/types/guild';
 
-export interface GuildTag {
+interface GuildTag {
   type: string;
   value: string;
 }
@@ -12,9 +13,30 @@ export interface GuildRequest {
   maxMembers: number;
   appid: number;
   isPublic: boolean;
-  guildImg: string;
+  fileType: FileType;
   tags: GuildTag[];
 }
+
+interface GuildResponse {
+  msg: string;
+  resultCode: string;
+  data: {
+    createdAt: string;
+    description: string;
+    guildImg: string;
+    id: number;
+    isPublic: boolean;
+    leaderImg: string;
+    leaderName: string;
+    maxMembers: number;
+    memberCount: number;
+    myRole: string;
+    name: string;
+    tags: string[];
+  };
+}
+
+// interface GuildPoplular
 
 type FileType = 'png' | 'jpg' | 'jpeg' | 'webp';
 
@@ -22,10 +44,26 @@ export const useGuild = () => {
   const axios = useAxios();
 
   async function GetGuild(guildId: string) {
-    const response = await axios.Get(GUILD.detail(guildId), {}, true);
-    const data = response?.data;
-
-    console.log(data);
+    const response = await axios.Get<GuildResponse>(GUILD.detail(guildId), {}, true);
+    const data = response?.data.data;
+    if (data) {
+      const guildDetail: guild = {
+        guild_name: data?.name,
+        description: data?.description,
+        img_src: data?.guildImg,
+        num_members: data?.memberCount,
+        owner: { username: 'test', nickname: data.leaderName, user_title: 'title', img_src: data.leaderImg },
+        created_at: new Date(data.createdAt),
+        myRole: data.myRole,
+        play_style: ['노멀', '도전과제'],
+        skill_level: ['뉴비', '마스터'],
+        gender: ['남자만'],
+        friendly: ['게임 전용'],
+      };
+      console.log(guildDetail);
+      return guildDetail;
+    }
+    return null;
   }
 
   async function UpdateGuild(guildId: string, newData: GuildRequest) {
@@ -93,7 +131,7 @@ export const useGuild = () => {
 
   async function UploadImageURL(fileType: FileType) {
     const response = await axios.Get(GUILD.upload_image, { params: { fileType: fileType } }, true);
-    const url = response?.data.data;
+    const url = response?.data;
     console.log(url);
     return url;
   }
