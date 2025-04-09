19d568dccd3df1778bb3b91bb2318342344badbb
marie4053 <marie4053@naver.com>
2025-04-10 01:06:27 +0900

    feat: guild 훅 정리

--- src/api/guild.ts
+++ src/api/guild.ts
@@ -1,11 +1,12 @@
 import { GUILD_ENDPOINTS as GUILD } from '@/constants/endpoints/guild';
 import { useAxios } from '@/hooks/useAxios';
 import { guild } from '@/types/guild';
-import { GuildRequest, GuildDetailResponse, Sort, GuildMainResponse } from '@/types/guildApi';
+import { GuildDetailResponse, Sort, GuildMainResponse, GuildUpdateRequest, GuildCreateRequest } from '@/types/guildApi';
 
 export const useGuild = () => {
   const axios = useAxios();
 
+  // ✅ 파싱 완료, 테스트 완료
   async function GetGuild(guildId: string) {
     const response = await axios.TypedGet<GuildDetailResponse>(GUILD.detail(guildId), {}, true);
     const data = response?.data;
@@ -30,18 +31,23 @@ export const useGuild = () => {
     return null;
   }
 
-  async function UpdateGuild(guildId: string, newData: GuildRequest) {
+  // ✅ ⚠️ 테스트 완료 (풀 받고 다시 확인해보기)
+  async function UpdateGuild(guildId: string, newData: GuildUpdateRequest) {
     const response = await axios.Put(GUILD.modify(guildId), newData, {}, true);
     const data = response?.data;
     console.log(data);
+    return data;
   }
 
+  // ✅ 테스트 완료
   async function DeleteGuild(guildId: string) {
     const response = await axios.Delete(GUILD.delete(guildId), {}, true);
     const data = response?.data;
     console.log(data);
+    return data;
   }
 
+  // ❌ 테스트 불가
   async function GetGuildList(request: object, page?: number, pageSize?: number, sort?: Sort) {
     const response = await axios.Get(
       GUILD.list,
@@ -59,23 +65,33 @@ export const useGuild = () => {
     console.log(data);
   }
 
-  async function CreateGuild(newData: GuildRequest) {
-    const response = await axios.Post(GUILD.create, newData, {}, true);
-    const data = response?.data;
-    console.log(data);
+  // ✅  테스트 완료
+  async function CreateGuild(newData: GuildCreateRequest) {
+    try {
+      const response = await axios.Post(GUILD.create, newData, {}, true);
+      const data = response?.data;
+      console.log(data);
+      return data;
+    } catch (error) {
+      console.log(error);
+    }
   }
 
+  // ❌ 테스트 불가 (로직 수정 중)
   async function UploadImageURL(guildId: string, url: string) {
     const response = await axios.Post(GUILD.upload_image(guildId), { url }, {}, true);
     console.log(response);
   }
 
+  // ✅  테스트 완료
   async function GetAdmin(guildId: string) {
     const response = await axios.Get(GUILD.admin(guildId), {}, true);
     const data = response?.data;
     console.log(data);
+    return data;
   }
 
+  // ✅ 파싱 완료, 테스트 완료
   async function GetGuildRecommend(appid: string, count?: number) {
     const response = await axios.TypedGet<GuildMainResponse>(
       GUILD.recommend,
@@ -112,6 +128,7 @@ export const useGuild = () => {
     return null;
   }
 
+  // ⚠️ 파싱 완료, 테스트 불가
   async function GetGuildPopular() {
     const response = await axios.TypedGet<GuildMainResponse>(GUILD.popular, {}, true);
     const data = response?.data;
