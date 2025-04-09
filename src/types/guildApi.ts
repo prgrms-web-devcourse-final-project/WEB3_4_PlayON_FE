3f61cd80019dc5aecd59a2cf79300ae3fd0877cd
marie4053 <marie4053@naver.com>
2025-04-09 22:32:43 +0900

    feat: guildApi 타입 생성

---
+++ src/types/guildApi.ts
@@ -0,0 +1,83 @@
+type FileType = 'png' | 'jpg' | 'jpeg' | 'webp';
+
+export type Sort = 'latest' | 'activity' | 'members';
+
+// 길드 생성&수정 시 필요
+export interface GuildRequest {
+  name: string;
+  description: string;
+  maxMembers: number;
+  appid: number;
+  isPublic: boolean;
+  fileType: FileType;
+  tags: GuildTag[];
+}
+
+interface GuildTag {
+  type: string;
+  value: string;
+}
+
+export interface GuildDetail {
+  createdAt: string;
+  description: string;
+  guildImg: string;
+  id: number;
+  isPublic: boolean;
+  leaderImg: string;
+  leaderName: string;
+  maxMembers: number;
+  memberCount: number;
+  myRole: string;
+  name: string;
+  tags: string[];
+}
+
+export interface GuildSimple {
+  guildId: number;
+  guildImg: string;
+  name: string;
+  description: string;
+  memberCount: number;
+  tags: string[];
+}
+
+export interface GuildDetailResponse {
+  msg: string;
+  resultCode: string;
+  data: GuildDetail;
+}
+
+export interface GuildListResponse {
+  msg: string;
+  resultCode: string;
+  data: {
+    currentPageNumber: number;
+    pageSize: number;
+    totalPages: number;
+    totalItems: number;
+    items: GuildSimple[];
+  };
+}
+
+export interface GuildAdminResponse {
+  resultCode: string;
+  msg: string;
+  data: {
+    id: 9007199254740991;
+    name: string;
+    leaderName: string;
+    managerNames: string[];
+    memberCount: 1073741824;
+    guildImg: string;
+    createdAt: string;
+    myRole: string;
+    tags: string[];
+  };
+}
+
+export interface GuildMainResponse {
+  resultCode: string;
+  msg: string;
+  data: GuildSimple[];
+}
