8648fc893c54fcee8a159e2169efe143312d1d50
SangYupKim <kylekim95@gmail.com>
2025-04-09 15:28:28 +0900

    feat : typeconverter

--- src/utils/typeConverter.ts
+++ src/utils/typeConverter.ts
@@ -1,39 +1,57 @@
 const typeMap = {
-  GAME_SKILL: {
-    koToEn: {
-      뉴비: 'CLEAN_WATER',
-      프로: 'MUD_WATER',
-      해커: 'STAGNANT_WATER',
-      마스터: 'ROTTEN_WATER',
+  playStyle: {
+    KoToEn: {
+      맛보기: 'BEGINNER',
+      캐주얼: 'CASUAL',
+      노멀: 'NORMAL',
+      하드: 'HARDCORE',
+      익스트림: 'EXTREME',
+      도전과제: 'COMPLETIONIST',
+      스피드러너: 'SPEEDRUN',
     },
-    enToKo: {
-      CLEAN_WATER: '뉴비',
-      MUD_WATER: '프로',
-      STAGNANT_WATER: '해커',
-      ROTTEN_WATER: '마스터',
-    },
-    paramToEnum: {
-      newbie: 'CLEAN_WATER',
-      pro: 'MUD_WATER',
-      hacker: 'STAGNANT_WATER',
-      master: 'ROTTEN_WATER',
-    },
-  },
-  GENDER: {
-    koToEn: {
-      여성: 'FEMALE',
-      남성: 'MALE',
-    },
-    enToKo: {
-      FEMALE: '여성',
-      MALE: '남성',
-    },
-    paramToEnum: {
-      female: 'FEMALE',
-      male: 'MALE',
+    EnToKo: {
+      BEGINNER: '맛보기',
+      CASUAL: '캐주얼',
+      NORMAL: '노멀',
+      HARDCORE: '하드',
+      EXTREME: '익스트림',
+      COMPLETIONIST: '도전과제',
+      SPEEDRUN: '스피드러너',
     },
   },
-} as const;
+  skillLevel: {
+    KoToEn: {
+      뉴비: 'NEWBIE',
+      프로: 'PRO',
+      해커: 'HACKER',
+      마스터: 'MASTER',
+    },
+    EnToKo: {
+      NEWBIE: '뉴비',
+      PRO: '프로',
+      HACKER: '해커',
+      MASTER: '마스터',
+    },
+  },
+  gender: {
+    KoToEn: {
+      남자만: 'MALE',
+      여자만: 'FEMALE',
+    },
+    EnToKo: {
+      MALE: '남자만',
+      FEMALE: '여자만',
+    },
+  },
+  friendly: {
+    KoToEn: {
+      '친목 환영': 'SOCIAL_FRIENDLY',
+      '게임 전용': 'GAME_ONLY',
+      '대화 없음': 'NO_CHAT',
+    },
+    EnToKo: {},
+  },
+};
 
 type TypeMap = typeof typeMap;
 type ConvertingType = keyof TypeMap;
