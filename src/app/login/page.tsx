cddb3c2aeecf90c0c29f90660d6b0a4455d499e7
SangYupKim <kylekim95@gmail.com>
2025-04-11 03:26:04 +0900

    fix : test 코드 제거

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -10,15 +10,11 @@ import { useForm } from 'react-hook-form';
 import { zodResolver } from '@hookform/resolvers/zod';
 import { Form, FormControl, FormField, FormItem, FormMessage } from '@/components/ui/form';
 import { useRouter } from 'next/navigation';
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
 import { PATH } from '@/constants/routes';
 import { useMembers } from '@/api/members';
 import { useToast } from '@/hooks/use-toast';
 import { useAuthStore } from '@/stores/authStore';
-import typeConverter from '@/utils/typeConverter';
-import { uploadToS3 } from '@/utils/uploadToS3';
-import { useGame } from '@/api/game';
-import { useGuildBoard } from '@/api/guildBoard';
 
 const loginSchema = z.object({
   email: z.string().min(1, { message: '아이디를 입력해주세요' }),
@@ -72,17 +68,8 @@ export default function LoginInitial() {
 
   const [submitHover, setSubmitHover] = useState(false);
 
-  const guildBoard = useGuildBoard();
   return (
     <div className="bg-purple-900 text-purple-400 w-full h-screen flex flex-col items-center mt-[68px]">
-      <button
-        onClick={async () => {
-          const response = await guildBoard.GuildPostDetail(2, 1);
-          console.log(response);
-        }}
-      >
-        Click Me!
-      </button>
       <div className="overlay pointer-events-none"></div>
       <div className="scanline pointer-events-none"></div>
       <div className="scrollanimation">
