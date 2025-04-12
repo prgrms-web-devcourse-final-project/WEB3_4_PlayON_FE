8fb32850aa96471ec0267c32f4398d6f82083656
SangYupKim <kylekim95@gmail.com>
2025-04-12 13:06:07 +0900

    feat update

--- src/api/game.ts
+++ src/api/game.ts
@@ -1,44 +1,45 @@
 import { GAME_ENDPOINTS } from '@/constants/endpoints/game';
 import { useAxios } from '@/hooks/useAxios';
 
+export type paging = {
+  page: number;
+  size: number;
+  sort: string[];
+};
+export type partyLog = {
+  id: number;
+  name: string;
+  partyAt: Date;
+  tags: string[];
+  memberCount: number;
+};
+export type party = {
+  id: number;
+  partyId: string;
+  name: string;
+  memberCount: number;
+  tags: string[];
+};
+export type game = {
+  appid: number;
+  name: string;
+  headerImage: string;
+  shortDescription: string;
+  aboutTheGame: string;
+  requiredAge: number;
+  website: string;
+  isWindowsSupported: true;
+  isMacSupported: true;
+  isLinuxSupported: true;
+  releaseDate: Date;
+  developers: string;
+  publishers: string;
+  screenshots: string[];
+  movies: string[];
+  genres: string[];
+};
+
 export const useGame = () => {
-  type paging = {
-    page: number;
-    size: number;
-    sort: string[];
-  };
-  type partyLog = {
-    id: number;
-    name: string;
-    partyAt: Date;
-    tags: string[];
-    memberCount: number;
-  };
-  type party = {
-    id: number;
-    partyId: string;
-    name: string;
-    memberCount: number;
-    tags: string[];
-  };
-  type game = {
-    appid: number;
-    name: string;
-    headerImage: string;
-    shortDescription: string;
-    aboutTheGame: string;
-    requiredAge: number;
-    website: string;
-    isWindowsSupported: true;
-    isMacSupported: true;
-    isLinuxSupported: true;
-    releaseDate: Date;
-    developers: string;
-    publishers: string;
-    screenshots: string[];
-    movies: string[];
-    genres: string[];
-  };
   const axios = useAxios();
 
   async function GamePartyList(appid: number, pageable?: paging) {
@@ -53,7 +54,7 @@ export const useGame = () => {
         items: response.data.data.items as party[],
       };
     }
-    return false;
+    throw new Error('Failed to fetch');
   }
   async function GamePartyLogList(appid: number, pageable?: paging) {
     const response = await axios.Get(GAME_ENDPOINTS.logs(appid), { params: { ...pageable } }, true);
@@ -79,7 +80,7 @@ export const useGame = () => {
         partyLogList: response.data.data.partyLogList as partyLog[],
       };
     }
-    return false;
+    throw new Error('Failed to fetch');
   }
   async function GameSearchByKeyword(keyword: string) {
     const response = await axios.Get(GAME_ENDPOINTS.search, { params: { keyword } }, true);
@@ -93,7 +94,7 @@ export const useGame = () => {
     if (response && response.status === 200) {
       return response.data.data as { appid: number; name: string; headerImage: string; genres: string[] }[];
     }
-    return false;
+    throw new Error('Failed to fetch');
   }
   async function GameMostPlayTime() {
     const response = await axios.Get(GAME_ENDPOINTS.playTime, {}, true);
@@ -107,7 +108,7 @@ export const useGame = () => {
     if (response && response.status === 200) {
       return response.data.data as { appid: number; name: string; headerImage: string; genres: string[] }[];
     }
-    return false;
+    throw new Error('Failed to fetch');
   }
   async function GameRanking() {
     const response = await axios.Get(GAME_ENDPOINTS.ranking, {}, true);
