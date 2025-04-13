df60ee406adab63a624b6b738378d378e7dad0c8
TaeHwan Kim <rainkdk02@gmail.com>
2025-04-13 23:28:53 +0900

    feat: 파티 로그 api

--- src/api/partyLog.ts
+++ src/api/partyLog.ts
@@ -4,31 +4,66 @@ import { PARTYLOG_ENDPOINTS as PARTYLOG } from '@/constants/endpoints/party-log'
 export const usePartyLog = () => {
   const axios = useAxios();
 
+  // 파티 로그 수정
   async function PutPartyLog(
     logId: string,
     partyId: string,
     data: { comment: string; content: string; deleteUrl: string; newFileType: string }
   ) {
-    const response = await axios.Put(PARTYLOG.modify(logId, partyId), { ...data }, {}, true);
-    console.log(response);
+    const response = await axios.Put(
+      PARTYLOG.modify(logId, partyId),
+      { ...data },
+      { headers: { 'Content-Type': 'application/json' } },
+      true
+    );
+    console.log(response?.data);
+    return response;
   }
+
+  // 파티 로그 삭제
+
   async function DeletePartyLog(logId: string, partyId: string) {
-    const response = await axios.Delete(PARTYLOG.delete(logId, partyId), {}, true);
+    const response = await axios.Delete(
+      PARTYLOG.delete(logId, partyId),
+      { headers: { 'Content-Type': 'application/json' } },
+      true
+    );
     console.log(response);
+    console.log(response?.data);
+    return response;
   }
-  async function SaveScreenshot(logId: string, partyId: string) {
-    const response = await axios.Post(PARTYLOG.screenshot(logId, partyId), {}, {}, true);
+
+  // 스크린샷 URL 저장
+  async function SaveScreenshot(logId: string, partyId: string, url: string) {
+    const response = await axios.Post(
+      PARTYLOG.screenshot(logId, partyId),
+      { url },
+      { headers: { 'Content-Type': 'application/json' } },
+      true
+    );
     console.log(response);
+    console.log(response?.data);
+    return response;
   }
-  async function GetPartyLogs(partyId: string) {
-    const response = await axios.Get(PARTYLOG.list(partyId), {}, true);
-    console.log(response);
-  }
+  
+  // 파티 로그 작성
   async function CreatePartyLog(
     partyId: string,
-    data: { comment: string; content: string; deleteUrl: string; newFileType: string }
+    data: { comment: string; content: string; fileType: string; partyMemberId: string }
   ) {
     const response = await axios.Post(PARTYLOG.write(partyId), { ...data }, {}, true);
     console.log(response);
+    console.log(response?.data);
+    return response;
   }
+  
+  // 파티의 모든 파티 로그 조회
+  async function GetPartyLogs(partyId: string) {
+    const response = await axios.Get(PARTYLOG.list(partyId), {}, true);
+    console.log(response);
+    return response;
+  }
+
+
+  return { PutPartyLog, DeletePartyLog, SaveScreenshot, GetPartyLogs, CreatePartyLog };
 };
