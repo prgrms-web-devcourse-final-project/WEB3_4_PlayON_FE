5dc87b02e21b0a0e1f6ed0d6acf5aa8b8121893e
jihye choi <jihyec22@gmail.com>
2025-04-14 17:04:14 +0900

    Refactor: 변수명 수정

--- src/components/common/EmptyLottie.tsx
+++ src/components/common/EmptyLottie.tsx
@@ -9,19 +9,19 @@ const Lottie = dynamic(() => import('react-lottie-player'), {
 
 type Props = {
   children?: ReactNode;
-  placeholder?: string;
+  text?: string;
   className?: string;
   imgWidth?: number;
 };
 
-export default function EmptyLottie({ children, placeholder, className, imgWidth = 380 }: Props) {
+export default function EmptyLottie({ children, text, className, imgWidth = 380 }: Props) {
   return (
     <div className={cn('relative text-center -translate-y-[18%] mx-auto', className)}>
       <p className="absolute font-dgm right-0 text-xl top-20 mt-8 w-1/3 text-purple-700">I Can&apos;t find Data...</p>
       <Lottie loop animationData={catAni} play className={`w-full max-w-[${imgWidth}px]`}></Lottie>
       <img src="/img/box_for_cat.svg" alt="box" className={`w-full max-w-[${imgWidth}px] absolute top-0 left-0`} />
       <div className="pl-8">
-        <p className="font-dgm text-2xl text-purple-800">{placeholder || '플레이어님을 기다리고 있어요!'}</p>
+        <p className="font-dgm text-2xl text-purple-800">{text || '플레이어님을 기다리고 있어요!'}</p>
         {children}
       </div>
     </div>
