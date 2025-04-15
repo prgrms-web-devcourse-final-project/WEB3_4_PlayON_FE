6eac562685046e6eaca7f7dd609f7a9d61d4f039
SangYupKim <kylekim95@gmail.com>
2025-04-15 11:49:22 +0900

    fix : 닉네임 제한

--- src/app/signup/userSchema.ts
+++ src/app/signup/userSchema.ts
@@ -5,7 +5,7 @@ export const userSchema = z.object({
   email: z.string().min(1, { message: '아이디를 입력해주세요' }),
   password: z.string().min(4, { message: '비밀번호는 최소 4글자 이상이어야합니다' }),
 
-  nickname: z.string().min(4, { message: '아이디는 최소 4글자 이상이어야합니다' }),
+  nickname: z.string().min(1, { message: '아이디는 최소 4글자 이상이어야합니다' }),
   avatar: z.string().nullable(),
   playStyle: z.enum([...userCategories.playStyle.items]),
   skillLevel: z.enum([...userCategories.skillLevel.items]),
