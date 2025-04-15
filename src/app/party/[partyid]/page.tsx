ef7fa56457f203dab47bcef5d1135ddcb8cd0a63
jihye choi <jihyec22@gmail.com>
2025-04-16 01:21:44 +0900

    Design: 파티 디테일 페이지 시각적 단차 제거

--- src/app/party/[partyid]/page.tsx
+++ src/app/party/[partyid]/page.tsx
@@ -19,13 +19,11 @@ function PartyPage() {
     fetchImages();
   }, [partyInfo]);
   return (
-    <div className="bg-purple-100 pt-28 pb-16 min-h-screen flex items-center">
+    <div className="bg-purple-100 pt-28 pb-16 min-h-screen h-full flex items-center overflow-hidden">
       <div
-        className={`${styles.background} w-full aspect-video absolute top-0 z-0`}
+        className={`${styles.background} w-full max-h-screen aspect-video absolute top-0 z-0`}
         style={{ backgroundImage: `url(${selectedGame})` }}
-      >
-        <div className="bg-gradient-to-b from-purple-50/0 to-purple-100 w-full h-1/2 absolute bottom-0"></div>
-      </div>
+      ></div>
       {partyInfo && (
         <div className="wrapper relative z-10 w-full">
           <div className="bg-white/50 backdrop-blur-md py-10 px-16 rounded-3xl relative border border-whit">
@@ -34,6 +32,7 @@ function PartyPage() {
           </div>
         </div>
       )}
+      <div className="bg-gradient-to-b from-purple-50/0 to-purple-100 w-full h-1/2 absolute bottom-0"></div>
     </div>
   );
 }
