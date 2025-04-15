5735cc8fa9abc104c600ac8987fbdcf984ffd122
TaeHwan Kim <rainkdk02@gmail.com>
2025-04-15 09:12:13 +0900

    feat: 마이페이지 기능구현

--- src/app/user/me/page.tsx
+++ src/app/user/me/page.tsx
@@ -17,7 +17,7 @@ import {
 } from '@/components/ui/pagination';
 import { gameSimple } from '@/types/games';
 import { guild } from '@/types/guild';
-import { party, partyLog } from '@/types/party';
+import { getPartyRes, party, partyLog } from '@/types/party';
 import {
   dummyGameSimple,
   dummyGuild,
@@ -27,91 +27,210 @@ import {
   dummyUserSimple,
 } from '@/utils/dummyData';
 import { ChevronLeft, ChevronRight, MailIcon, Pencil, SquarePen } from 'lucide-react';
-import { useState } from 'react';
-import { EditInfo } from './components/MyModal';
+import { useEffect, useState } from 'react';
 import SteamSVG from '@/components/svg/steam';
 import Tag from '@/components/common/Tag';
+import { useMembers } from '@/api/members';
+import { userDetail } from '@/types/user';
+import { useRouter } from 'next/navigation';
+import { useAuthStore } from '@/stores/authStore';
+import Link from 'next/link';
+
+interface userMe {
+  gerder: string;
+  img_src?: string;
+  last_login_at: Date; // Mon Apr 14 2025 09:45:15 GMT+0900 (한국 표준시) {}
+  nickname: string;
+  party_style: string[];
+  skill_level: string;
+  steam_id?: number;
+  user_title?: string[];
+  username: string;
+}
+
+interface resGameProps {
+  gameData: {
+    title: string;
+    genre: string[];
+    img_src: string;
+    background_src: '';
+    appid: number;
+  };
+}
 
 export default function MyPage() {
   const dummyGuildList: guild[] = Array(3).fill(dummyGuild);
   const dummyPartyList: party[] = Array(3).fill(dummyParty);
   const dummyPartyLogList: partyLog[] = Array(6).fill(dummyPartyLog);
+  const dummyGameArr = new Array<gameSimple>(8).fill(dummyGameSimple);
 
   const [api, setApi] = useState<CarouselApi>();
 
-  const dummyGameArr = new Array<gameSimple>(8).fill(dummyGameSimple);
-  const [selectedGame, setSelectedGame] = useState<number>(0);
-  const isSteamToken = 'abc';
-  const playStyle = dummyUserDetail.party_style;
-  const skillLevel = dummyUserDetail.skill_level;
-  const gender = dummyUserDetail.gender;
+  const memberApi = useMembers();
+  const [userMe, SetUserMe] = useState<userDetail | null>(null);
+  const [myGames, setMyGames] = useState<resGameProps[] | null>([]);
+  const [myGuilds, setMyGuilds] = useState<guild[] | null>([]);
+  const [myParties, setMyParties] = useState<getPartyRes[] | null>([]);
+  const [myPartyLogs, setMyPartyLogs] = useState<getPartyRes[] | null>([]);
+
+
+  useEffect(() => {
+    const GetMe = async () => {
+      try {
+        const res = await memberApi.GetMe();
+        console.log('응답 Me', res);
+        SetUserMe(res);
+        console.log('userMe', userMe);
+      } catch (error: any) {
+        console.log('에러 발생', error.response);
+      }
+    };
+    GetMe();
+
+    const MyGames = async () => {
+      try {
+        const res = await memberApi.MyGames();
+
+        console.log('mygame 응답', res);
+
+        setMyGames(res);
+        console.log('상태 응답', myGames);
+      } catch (error: any) {
+        console.log('에러 발생', error.response);
+      }
+    };
+    MyGames();
+
+    const MyGuilds = async () => {
+      try {
+        const res = await memberApi.MyGuilds();
+
+        console.log('myguilds 응답', res);
+
+        setMyGuilds(res);
+        console.log('상태 응답', myGames);
+      } catch (error: any) {
+        console.log('에러 발생', error.response);
+      }
+    };
+    MyGuilds();
+
+    const GetMyParties = async () => {
+      try {
+        const res = await memberApi.GetMyParties();
+        console.log('파티응답', res);
+        setMyParties(res);
+      } catch (error: any) {
+        console.log('에러 발생 파티1', error);
+      }
+    };
+    GetMyParties();
+
+    const GetMyPartyLogs = async () => {
+      try {
+        const res = await memberApi.GetMyPartyLogs();
+        console.log('페이지 단 파티로그응답', res);
+        setMyPartyLogs(res);
+      } catch (error: any) {
+        console.log('에러 발생 파티', error);
+      }
+    };
+    GetMyPartyLogs();
+  }, []);
+
+  // if (!userMe) return <div className="text-center pt-28">로딩 중...</div>;
+
+  // const defauiltAvatar = '/img/_defilmmopruy.jpg';
+  const defauiltAvatar = '/img/dummy_profile.jpg';
+
+  const router = useRouter();
+  const member = useMembers();
+  const { setUser } = useAuthStore();
+
+  async function steamAuth() {
+    const response = await member.steamAuthLink();
+    window.location.href = response;
+  }
+
+  const defualt: string = "네가 제일 잘 나가";
 
   return (
     <main>
       <section className="wrapper pt-36">
         <div className="flex flex-col gap-16">
           <div className="w-full px-8 py-9 border border-neutral-300 rounded-2xl">
-            <div className="flex gap-7 relative">
-              <Avatar className="bg-neutral-400 w-24 h-24">
-                <AvatarImage src={dummyUserSimple.img_src} />
-              </Avatar>
+            {userMe ? (
+              <div className="flex gap-7 relative">
+                <Avatar className="bg-neutral-400 w-24 h-24">
+                  {/* <AvatarImage src={ (userMe.img_src ? (userMe.img_src) : (defauiltAvatar) )}  /> */}
+                  <AvatarImage src={userMe.img_src ?? defauiltAvatar} />
+                </Avatar>
 
-              <div>
-                <div className="flex gap-3">
-                  <p className="font-suit text-2xl font-semibold text-neutral-400">{dummyUserSimple.user_title}</p>
-                  <p className="font-suit text-2xl font-bold ">{dummyUserSimple.nickname}</p>
-                </div>
-
-                <div className="flex gap-8">
-                  <div className="font-suit text-base font-semibold text-neutral-400 flex">
-                    스팀 아이디 :&nbsp;
-                    {isSteamToken ? (
-                      <div>{dummyUserSimple.username.split('@')[0]}</div>
-                    ) : (
-                      <button className="flex flex-row gap-1">
-                        <SteamSVG fill={'#8258ff'} stroke="" width={24} height={24} />
-                        <p className="text-base font-black text-purple-400">STEAM</p>
-                      </button>
-                    )}
+                <div>
+                  <div className="flex gap-3">
+                    <p className="font-suit text-2xl font-semibold text-neutral-400">{userMe.user_title ?? defualt} </p>
+                    <p className="font-suit text-2xl font-bold ">{userMe.nickname}</p>
                   </div>
-                  <p className="font-suit text-base font-semibold text-neutral-400"> 성별 : {dummyUserDetail.gender}</p>
-                </div>
 
-                <div className="gap-5">
-                  <p className="font-suit text-2xl font-bold pt-8">{dummyUserSimple.nickname} 님의 플레이 스타일</p>
+                  <div className="flex gap-8">
+                    <div className="font-suit text-base font-semibold text-neutral-400 flex">
+                      스팀 아이디 :&nbsp;
+                      {userMe.steam_id ? (
+                        <div>{userMe.steam_id}</div>
+                      ) : (
+                        // <div>{userMe.steam_id.split('@')[0]}</div>
+                        <button className="flex flex-row gap-1" onClick={() => steamAuth()}>
+                          <SteamSVG fill={'#8258ff'} stroke="" width={24} height={24} />
+                          <p className="text-base font-black text-purple-400">STEAM</p>
+                        </button>
+                      )}
+                    </div>
+                    <p className="font-suit text-base font-semibold text-neutral-400"> 성별 : {userMe.gender}</p>
+                  </div>
 
-                  <div className="flex flex-col rounded-xl gap-2 py-6">
-                    <div className="flex items-center gap-2">
-                      <p className="w-[118px] font-dgm text-neutral-900">플레이 스타일</p>
+                  <div className="gap-5">
+                    <p className="font-suit text-2xl font-bold pt-8">{userMe.nickname} 님의 플레이 스타일</p>
+
+                    <div className="flex flex-col rounded-xl gap-2 py-6">
+                      <div className="flex items-center gap-2">
+                        <p className="w-[118px] font-dgm text-neutral-900">플레이 스타일</p>
+                        <div className="flex gap-2">
+                          <Tag style="retro" size="small" background="dark">
+                            {userMe.party_style}
+                          </Tag>
+                        </div>
+                      </div>
                       <div className="flex gap-2">
+                        <p className="w-[118px] font-dgm text-neutral-900">게임 실력</p>
                         <Tag style="retro" size="small" background="dark">
-                          {playStyle}
+                          {userMe.skill_level}
+                        </Tag>
+                      </div>
+                      <div className="flex gap-2">
+                        <p className="w-[118px] font-dgm text-neutral-900">성별</p>
+                        <Tag style="retro" size="small" background="dark">
+                          {userMe.gender}
                         </Tag>
                       </div>
                     </div>
-                    <div className="flex gap-2">
-                      <p className="w-[118px] font-dgm text-neutral-900">게임 실력</p>
-                      <Tag style="retro" size="small" background="dark">
-                        {skillLevel}
-                      </Tag>
-                    </div>
-                    <div className="flex gap-2">
-                      <p className="w-[118px] font-dgm text-neutral-900">성별</p>
-                      <Tag style="retro" size="small" background="dark">
-                        {gender}
-                      </Tag>
-                    </div>
+                  </div>
+                </div>
+                <div className="absolute -top-1 right-0">
+                  <div className="w-full">
+                    {/* <EditInfo /> */}
+
+                    <Link href="/user/me/modify">
+                      <SquarePen color="#A3A3A3" />
+                    </Link>
                   </div>
                 </div>
               </div>
-              <div className="absolute -top-1 right-0">
-                <div className="w-full">
-                  <EditInfo />
-                </div>
-              </div>
-            </div>
+            ) : (
+              <p>유저를 불러오는중</p>
+            )}
           </div>
 
+          {/* 나의 길드 */}
           <div className="flex flex-col gap-6">
             <p className="font-suit text-3xl font-semibold">나의 길드</p>
 
@@ -131,9 +250,7 @@ export default function MyPage() {
                       return (
                         <CarouselItem key={ind} onClick={() => setSelectedGame(ind)} className={``}>
                           <div className="grid grid-cols-3 gap-6">
-                            {dummyGuildList.map((guild) => (
-                              <GuildHorizon key={guild.guild_name} data={guild} />
-                            ))}
+                            {myGuilds?.map((guild) => <GuildHorizon key={guild.guild_name} data={guild} />)}
                           </div>
                         </CarouselItem>
                       );
@@ -172,32 +289,36 @@ export default function MyPage() {
             </div>
           </div>
 
+          {/* 내가 보유한 게임 목록 */}
           <div className="flex flex-col gap-6">
             <p className="font-suit text-3xl font-semibold">내가 보유한 게임 목록</p>
             <div className="flex gap-6">
+              {myGames?.map((data) => <PopularCard key={data.gameData.appid} data={data.gameData} />)}
+              {/* <PopularCard data={dummyGameSimple} />
               <PopularCard data={dummyGameSimple} />
-              <PopularCard data={dummyGameSimple} />
-              <PopularCard data={dummyGameSimple} />
+              <PopularCard data={dummyGameSimple} /> */}
             </div>
           </div>
 
+          {/* 참여중인 파티 */}
           <div className="flex flex-col gap-6">
             <p className="font-suit text-3xl font-semibold">참여중인 파티</p>
             <div className="flex">
               <div className="grid grid-cols-3 gap-6">
-                {dummyPartyList.map((party) => (
-                  <PartyCard key={party.party_name} data={party} />
-                ))}
+                {myParties?.map((party) => <PartyCard key={party.partyId} data={party} />)}
+                {dummyPartyList?.map((party) => <PartyCard key={party.partyId} data={party} />)}
               </div>
             </div>
           </div>
 
+          {/* 참여한 파티 로그 */}
           <div className="flex flex-col gap-6">
             <p className="font-suit text-3xl font-semibold">참여한 파티 로그</p>
             <div className="grid grid-cols-3 gap-6">
-              {dummyPartyLogList.map((partyLog) => (
+              {myPartyLogs?.map((partyLog) => <PartyLogCard key={partyLog.partyId} data={partyLog} />)}
+              {/* {dummyPartyLogList.map((partyLog) => (
                 <PartyLogCard key={partyLog.party_info.party_name} data={partyLog} />
-              ))}
+              ))} */}
             </div>
           </div>
 
