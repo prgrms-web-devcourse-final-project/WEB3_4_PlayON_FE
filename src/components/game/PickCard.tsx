a71e51f03395bc5fe3131476003adf5e0aa2f992
Sang Yup Kim <kylekim95@gmail.com>
2025-04-17 12:30:21 +0900

    352 feat party 카드 텍스트 너무 길게 출력되면 자르는 기능 (#354)
    * fix : pickcard 글자 제한 & 영상 정보 위로
    
    * fix pick card padding & test코드 삭제
    
    * fix : pickcard 글자 제한 & 영상 정보 위로
    
    * fix pick card padding & test코드 삭제

--- src/components/game/PickCard.tsx
+++ src/components/game/PickCard.tsx
@@ -15,8 +15,12 @@ export default function PickCard(props: PickCardProps) {
       <Link href={GAME_ROUTE.game_detail(props.appid)}>
         {/* <div className="bg-neutral-400 w-full aspect-square  rounded-full"></div> */}
         <img src={data.screenshot_src[0]} className="bg-neutral-400 w-full aspect-square rounded-full object-cover" />
-        <p className="mt-4 font-suit text-xl font-semibold"> {data.title}</p>
-        <p className="mt-2 text-sm text-neutral-400 font-medium flex"> {data.genre.join(', ')} </p>
+        <div className="pr-2">
+          <p className="mt-4 font-suit text-xl font-semibold text-ellipsis overflow-hidden">{data.title}</p>
+          <p className="mt-2 text-sm text-neutral-400 font-medium flex text-ellipsis overflow-hidden">
+            {data.genre.join(', ')}
+          </p>
+        </div>
       </Link>
     </>
   );
