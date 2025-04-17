e5fce32ac3cedd3287d091cf2295f16d5fe68bab
SangYupKim <kylekim95@gmail.com>
2025-04-17 11:33:23 +0900

    fix : pickcard 글자 제한 & 영상 정보 위로

--- src/components/game/PickCard.tsx
+++ src/components/game/PickCard.tsx
@@ -15,8 +15,14 @@ export default function PickCard(props: PickCardProps) {
       <Link href={GAME_ROUTE.game_detail(props.appid)}>
         {/* <div className="bg-neutral-400 w-full aspect-square  rounded-full"></div> */}
         <img src={data.screenshot_src[0]} className="bg-neutral-400 w-full aspect-square rounded-full object-cover" />
-        <p className="mt-4 font-suit text-xl font-semibold"> {data.title}</p>
-        <p className="mt-2 text-sm text-neutral-400 font-medium flex"> {data.genre.join(', ')} </p>
+        <p className="mt-4 font-suit text-xl font-semibold text-ellipsis overflow-hidden">
+          {' '}
+          {data.title + data.title + data.title}
+        </p>
+        <p className="mt-2 text-sm text-neutral-400 font-medium flex text-ellipsis overflow-hidden">
+          {' '}
+          {data.genre.join(', ')}{' '}
+        </p>
       </Link>
     </>
   );
